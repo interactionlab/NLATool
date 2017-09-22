3196b4c61541af3dc38cba26e5fd982fcf5d4917
telion <telion.official1@hotmail.com>
2017-09-22 12:00:26 +0200

    Bugfehler durch Umbenennung behoben. Xampp wegen third-Party Lizenz gelöscht.

---
+++ NLATool-Projekt/routes/signIn.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+//--------------------------------------------------------
+/**
+ * Tag für Konsolen Fehlersuche:
+ * @type {string}
+ */
+var desktop = 'desktop Version: ';
+var mobile = 'Mobile Version: ';
+var bigDesktop = 'Big Desktop Version: ';
+var notMedia = 'Not Media-Related Part: ';
+var Tag = 'signIn.js: ';
+
+
+module.exports = router;
--- xampp/perl/vendor/lib/Params/ValidatePP.pm
+++
@@ -1,9 +0,0 @@
-package Params::Validate;
-{
-  $Params::Validate::VERSION = '1.07';
-}
-
-BEGIN { $ENV{PARAMS_VALIDATE_IMPLEMENTATION} = 'PP' }
-use Params::Validate;
-
-1;
