b2621ba9c914c60e41b2164c27b0d8f2cc7b7200
Florin <florin12@gmx.de>
2017-10-01 21:33:41 +0200

    added loadtext route & view

--- NLATool-Projekt/routes/loadtext.js
+++ NLATool-Projekt/routes/loadtext.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var corenlp = require("corenlp-request-wrapper");
 var router = express.Router();
 
 //--------------------------------------------------------
@@ -15,8 +16,31 @@ var Tag = 'index.js: ';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('./Desktop/loadtext.js', {title: 'NLA - Natural Language Analyse Tool'});
+    res.render('./Desktop/loadtext', {title: 'NLA - Natural Language Analyse Tool', result:''});
 });
+
+router.post('/nlp2', function (req, res) {
+
+    var input = req.body.testFunction;
+    console.log(input);
+
+    //TODO: check if CoreNLP server is online
+
+    //start server with command
+    //java -mx1g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+    //where -mx_g is the number of GBs of RAM you want to allocate
+
+    corenlp.parse(
+        input, 9000, "pos,lemma,ner", "json", function (err, parsedText) {
+            console.log(JSON.stringify(JSON.parse(parsedText), null, 2));
+            res.render('Desktop/loadtext', {
+                title: 'NLA - Natural Language Analyse Tool',
+                result: JSON.stringify(JSON.parse(parsedText))
+            })
+        });
+
+});
+
 
 
 module.exports = router;
