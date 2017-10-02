b2b4f2f6c200c85cbe7546852c716d36064fcf99
Florin <florin12@gmx.de>
2017-10-02 22:37:33 +0200

    created start_server.bat

---
+++ NLATool-Projekt/start_server.bat
@@ -0,0 +1,3 @@
+cd C:\Users\Florin\Documents\stanford-corenlp-full-2016-10-31\stanford-corenlp-full-2016-10-31
+java -mx1g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 4000 -timeout 15000
+cmd
