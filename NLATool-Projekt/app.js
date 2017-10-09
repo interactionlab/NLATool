d21ef3d6ee60833204c4859a6acfc7988239f616
Florin <florin12@gmx.de>
2017-10-09 11:49:06 +0200

    fixed commenting error & css links

--- NLATool-Projekt/app.js
+++ NLATool-Projekt/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname,'css')));
+//app.use(express.static(path.join(__dirname,'css')));
 
 app.use('/', index);
 app.use('/analyse', analyse);
