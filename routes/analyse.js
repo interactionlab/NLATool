a357a43bed3cd188294df117c3f8b05ccce9cd60
telion2 <telion.official1@hotmail.com>
2017-10-23 13:02:52 +0200

    makeSQLRequest wont check the dbStatus for security anymore, reading Input and sending it to routes/analyse.js.

--- routes/analyse.js
+++ routes/analyse.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var dbStub = require('../modules/DB-Stub');
+var dbAction = require('../modules/DB-Actions');
 
 //--------------------------------------------------------
 /**
@@ -13,6 +15,13 @@ var notMedia = 'Not Media-Related Part: ';
 var Tag = 'analyse.js: ';
 
 router.get('/', function (req, res, next) {
-    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool'});
+    dbStub.fiberEstablishConnection();
+    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result:''});
+});
+
+router.post('/inputText', function (req, res) {
+    var text = req.body.textInput;
+    dbStub.makeSQLRequest('INSERT INTO nlatool . text');
+    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result:text});
 });
 module.exports = router;
