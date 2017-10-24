a2ae9ea1eb62a34b77d708a20203ade9bed94389
telion2 <telion.official1@hotmail.com>
2017-10-24 14:49:58 +0200

    tidied up ejs files so that scripts refer to a js file in in the public folder. Also started implementing material.io into loadtext.ejs

---
+++ public/javascripts/uiFunctions.js
@@ -0,0 +1,17 @@
+
+
+function expand() {
+    var button = document.getElementById('switch');
+    var rightDiv = document.getElementById('right-column');
+    if (button.value === 'v') {
+        button.value = '^';
+        rightDiv.style.display = 'inline';
+    } else {
+        button.value = 'v';
+        rightDiv.style.display = 'none';
+    }
+
+    function showMessage(value) {
+        document.getElementById("message").innerHTML = value;
+    }
+}
