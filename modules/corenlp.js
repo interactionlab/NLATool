d2bc496390dff4d3ca4186aa9ac2c4ae5a3ee0ba
telion2 <telion.official1@hotmail.com>
2017-10-26 11:52:40 +0200

    work on corenlp.js

--- modules/corenlp.js
+++ modules/corenlp.js
@@ -13,9 +13,11 @@ var sql = 'The resulting SQL Command is:';
  */
 var jsonConfigurator = require('jsonfile');
 var wait = require('wait.for');
-var corenlp = require('corenlp');
+//var corenlp = require('corenlp');
 var jsonAction = require('../modules/jsonActions');
 const isReachable = require('is-reachable');
+var coreNLP = require('../modules/corenlp');
+var wait = require('wait.for');
 //import CoreNLP, { Properties, Pipeline, ConnectorServer } from 'corenlp';
 /**
  * Loads the Database Configuration at the beginning of the of this file so
@@ -23,41 +25,90 @@ const isReachable = require('is-reachable');
  */
 var json = null;
 wait.launchFiber(getJSONConfig);
+
 function getJSONConfig() {
     json = jsonAction.getJsonConfiguration();
     json = JSON.parse(json);
 }
 
+/**
+ * Global Variables for this Document.
+ */
 var nlpStatus = {
-    reachable: false
+    reachable: false,
+    host: null,
+    error: null
 };
 
 /**
  * Reachability Check of Corenlp Server.
  */
-exports.isReachable = function (res, next) {
-    var reached = isReachable('http://projects.hcilab.org/CoreNLP/');
-    reached.then(function (reached) {
-        console.log(reached);
-        if (reached) {
+exports.isReachable = function (host, callback) {
+    if (!coreNLP.positiveNlpStatus()) {
+        var reached = isReachable(host);
+        reached.then(function (reached) {
+            console.log(reached);
+            if (reached) {
+                nlpStatus.reachable = reached;
+                nlpStatus.host = host;
+                callback(null, reached);
+            } else {
+                nlpStatus.reachable = reached;
+                var err = new Error('CoreNlp not reachable');
+                err.status = 502;
+                nlpStatus.error = err;
+                callback(err, reached);
+            }
+        }).catch(function (e) {
             nlpStatus.reachable = reached;
-            res.render('./Desktop/loadtext', {title: 'NLA - Natural Language Analyse Tool', result: ''});
-        } else {
-            nlpStatus.reachable = reached;
-            var err = new Error('CoreNlp not reachable');
-            err.status = 501;
-            next(err);
+            var err = new Error('CoreNlp not reachable' + e);
+            err.status = 502;
+            nlpStatus.error = err;
+            callback(err, null);
+        });
+    }
+};
+/**
+ * checks all connections specified in config.json if they are reachable.
+ */
+exports.getAReachableConnection = function () {
+    for (var connection in json.corenlp.connections) {
+        console.log(json.corenlp.connections[connection].host);
+        if (!coreNLP.positiveNlpStatus()) {
+            try {
+                wait.for(coreNLP.isReachable, json.corenlp.connections[connection].host);
+            }catch (e){}
+            if (nlpStatus.host !== null && nlpStatus.reachable === true) {
+                console.log(nlpStatus.host);
+            }
+
         }
-    }).catch(function (e) {
-        nlpStatus.reachable = reached;
-        var err = new Error('CoreNlp not reachable' + e);
-        err.status = 502;
-        next(err);
-    });
+    }
 };
 
-const connector = new ConnectorServer({ dsn: 'http://localhost:9000' });
-const props = new Properties({
-    annotators: 'tokenize,ssplit,pos,lemma,ner,parse'
-});
-const pipeline = new Pipeline(props, 'English', connector);
+exports.setupCorenlp = function () {
+    /*const connector = new ConnectorServer({ dsn: nlpStatus.host });
+    const props = new Properties({
+        annotators: 'tokenize,ssplit,pos,lemma,ner,parse'
+    });
+    const pipeline = new Pipeline(props, 'English', connector);*/
+};
+
+
+//--------------------------------------------------------
+/**
+ * Section for managing nlpStatus
+ */
+exports.getNlpStatus = function () {
+    return nlpStatus;
+};
+
+exports.resetNlpStatus = function () {
+    nlpStatus.error = null;
+    nlpStatus.reachable = false;
+    nlpStatus.host = null;
+};
+
+exports.positiveNlpStatus = function () {
+    return nlpStatus.host !== null && nlpStatus.reachable === true && nlpStatus.error === null;
+};
