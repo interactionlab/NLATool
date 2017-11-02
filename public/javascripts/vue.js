115c78951658479cb068c9eeb1f3e06cd4c63ff5
turcanee <emine@live.com.my>
2017-11-02 11:46:20 +0100

    first Vue application but not work

---
+++ public/javascripts/vue.js
@@ -0,0 +1,32 @@
+// 0. If using a module system, call Vue.use(VueRouter)
+
+// 1. Define route components.
+// These can be imported from other files
+const One = { template: '<div>one</div>' }
+const Two = { template: '<div>two</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+    { path: '/one', component: One },
+    { path: '/two', component: Two }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+    routes
+})
+
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+    router
+}).$mount('#app')
+
+// Now the app has started!
