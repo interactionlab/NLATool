e0290081ef5fca9d5e925add960cdabdbac2cb11
telion2 <telion.official1@hotmail.com>
2017-11-03 11:43:47 +0100

    moved Comment.ejs to views. added title variable tag to Comment.ejs, added route file for Comment.ejs and added basic elements or a route.

---
+++ routes/Comment.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+//--------------------------------------------------------
+/**
+ * Tags for console Errors:
+ * @type {string}
+ */
+let desktop = 'desktop Version: ';
+let mobile = 'Mobile Version: ';
+let bigDesktop = 'Big Desktop Version: ';
+let notMedia = 'Not Media-Related Part: ';
+let Tag = 'signUp.js: ';
+
+router.get('/', function (req, res, next) {
+    res.render('./Desktop/Comment', {title: 'NLA - Natural Language Analyse Tool'});
+});
+
+module.exports = router;
