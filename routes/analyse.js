99275845af04ce6521b21eead26e71c778560b58
telion2 <telion.official1@hotmail.com>
2017-11-04 17:42:49 +0100

    implemented a way to get the text from Db and send it to the client.

--- routes/analyse.js
+++ routes/analyse.js
@@ -18,7 +18,12 @@ const dbStub = require('../modules/DB-Stub');
 const dbAction = require('../modules/DB-Actions');
 const wait = require('wait.for');
 
-let results = [];
+let textDB = {
+    textMap: [],
+    textMetaData: [],
+    wordList: [],
+    error: []
+};
 
 router.get('/', function (req, res, next) {
     dbStub.fiberEstablishConnection();
@@ -32,18 +37,39 @@ router.post('/showText', function (req, res, next) {
 function getAndShowText(req, res, next) {
     let queryOperators = dbAction.getQueryOperators();
     let docID = '';
-    console.log(notMedia + Tag + 'Document Id from Session is: '+req.session.docID);
-    if (isNaN(req.session.docID)) {
+    console.log(notMedia + Tag + 'Document Id from Session is: ' + req.session.docID);
+    if (!isNaN(req.session.docID)) {
+        console.log('got here2');
         let docID = req.session.docID;
         getTextFromDB(docID);
     }
-    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result: ''});
+    console.log('got here1');
+    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result: JSON.stringify(textDB.wordList)});
 }
 
 function getTextFromDB(docID) {
-    let textMap = wait.for(dbStub.makeSQLRequest,
-        dbAction.createSelectCommand('textWords', ['wordID', 'docID', 'counter'], docID, ['=']));
-    console.log(notMedia + Tag + 'Result of selecting text in textWords: '+ textMap);
+    textDB.textMap = JSON.parse(wait.for(dbStub.makeSQLRequest,
+        dbAction.createSelectCommand('textWords', ['docID', 'wordID', 'counter'], [docID], ['='])));
+    //console.log(notMedia + Tag + 'Result of selecting text in textWords: ' + JSON.stringify(textDB.textMap));
+    for (let i = 0; i < textDB.textMap.length; i++) {
+        if (textDB.textMap[i].counter === i) {
+            let word = wait.for(dbStub.makeSQLRequest,
+                dbAction.createSelectCommand('word',
+                    ['wordID', 'content', 'isSpecial', 'semanticClass', 'pos'],
+                    [textDB.textMap[i].wordID], ['=']));
+            textDB.wordList.push(JSON.parse(word));
+        } else {
+            let err = new Error('Iteration is not synchronized with the counter attribute of the textMap.');
+            textDB.error.push(err);
+            break;
+        }
+    }
+    //console.log(notMedia + Tag + 'the current Word List:' + JSON.stringify(textDB.wordList));
+}
+
+function getTextMetaData(docID) {
+    textDB.textMetaData = wait.for(dbStub.makeSQLRequest,
+        dbAction.createSelectCommand('text', ['docID', 'length', 'title', 'author', 'year'], [docID], ['=']));
 }
 
 module.exports = router;
