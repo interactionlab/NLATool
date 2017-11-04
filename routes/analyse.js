c4bcc8ce8da53f0a20cb5248de4329291f66f83b
telion2 <telion.official1@hotmail.com>
2017-11-04 14:16:01 +0100

    debugged session, deleted an old comment, work on analyse.js

--- routes/analyse.js
+++ routes/analyse.js
@@ -22,27 +22,27 @@ let results = [];
 
 router.get('/', function (req, res, next) {
     dbStub.fiberEstablishConnection();
-    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result: ''});
+    wait.launchFiber(getAndShowText, req, res, next);
 });
 
 router.post('/showText', function (req, res, next) {
-    wait.launchFiber(postShowText, req, res, next);
+    res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result: ''});
 });
 
-function postShowText(req, res, next) {
+function getAndShowText(req, res, next) {
     let queryOperators = dbAction.getQueryOperators();
     let docID = '';
+    console.log(notMedia + Tag + 'Document Id from Session is: '+req.session.docID);
     if (isNaN(req.session.docID)) {
         let docID = req.session.docID;
+        getTextFromDB(docID);
     }
-
-    getTextFromDB(docID);
     res.render('./Desktop/analyse', {title: 'NLA - Natural Language Analyse Tool', result: ''});
 }
 
 function getTextFromDB(docID) {
     let textMap = wait.for(dbStub.makeSQLRequest,
-        dbAction.createSelectCommand('textWords', null, docID, ['=']));
+        dbAction.createSelectCommand('textWords', ['wordID', 'docID', 'counter'], docID, ['=']));
     console.log(notMedia + Tag + 'Result of selecting text in textWords: '+ textMap);
 }
 
