b69456c1a32f06bf2f4c009d7a455fa527e3e5e9
turcanee <emine@live.com.my>
2017-11-06 12:03:12 +0100

    change datanames

--- routes/signup.js
+++ routes/signup.js
@@ -10,7 +10,7 @@ let desktop = 'desktop Version: ';
 let mobile = 'Mobile Version: ';
 let bigDesktop = 'Big Desktop Version: ';
 let notMedia = 'Not Media-Related Part: ';
-let Tag = 'signUp.js: ';
+let Tag = 'signup.js: ';
 
 router.get('/', function (req, res, next) {
     res.render('./Desktop/signup', {title: 'NLA - Natural Language Analyse Tool'});
