32ae62363987394946b6a45908245196d6484507
GustavMurawski <gustav.murawski@gmail.com>
2017-11-08 22:14:04 +0100

    Router-Funktionen angepasst

--- routes/comment.js
+++ routes/comment.js
@@ -1,36 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-
 router.get('/', function (req, res, next) {
-    /*
-    id=id
-
-    return:
-    {
-        word: word,
-        comment: comment
-    }
-    */
+    let uid = req.body.uid;
+    let id = req.body.id;
+    let word = req.session.wordTables[uid].words[id];
+    let comment = req.session.wordTables[uid].comments[id];
+    res.json({word: word, comment: comment});
 });
+
 
 router.post('/save', function (req, res, next) {
-    /*
-    {
-        id: id,
-        comment: comment
-    }
-    */
+    let uid = req.body.uid;
+    let id = req.body.id;
+    let comment = req.body.comment;
+    req.session.wordTables[uid].comments[id] = comment;
+    res.json({});
 });
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
 
 router.post('/init', function (req, res, next) {
-    /*
-    {
-        words: ["word1", "word2", "word3"]
+    let uid = guid();
+    let words = req.body.words;
+    let comments = new Array(words.length);
+    if (!req.session.wordTables) {
+        req.session.wordTables = {};
     }
-    */
+    req.session.wordTables[uid] = {words: words, comments: comments};
+    res.json({uuid: uuid});
 });
-
 
 
 
