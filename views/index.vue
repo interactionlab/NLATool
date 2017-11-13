3824cf63872b646cb3415949eeadff5984ba10cc
telion2 <telion.official1@hotmail.com>
2017-11-13 14:22:12 +0100

    first try for new template

---
+++ views/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <html>
+    <head>
+        <title>{{ title }}</title>
+        <link rel='stylesheet' href='/css/style.css'/>
+    </head>
+    <body>
+    <h1>{{ title }}</h1>
+    <p>Welcome to {{ title }}</p>
+    </body>
+    </html>
+</template>
+
+<!--
+<script>
+    import messageComp from './components/message-comp.vue';
+    import users from './components/users.vue';
+    import exampleMixin from '../mixins/exampleMixin';
+    export default {
+        mixins: [exampleMixin],
+        data: function () {
+            return {
+            }
+        },
+        components: {
+            messageComp: messageComp,
+            users: users
+        }
+    }
+</script> -->
