ab7ef17048a0b0143b40a013ad8c12df492b9041
turcanee <emine@live.com.my>
2017-11-17 14:35:19 +0100

    changed signinto vue

--- routes/signin.js
+++ routes/signin.js
@@ -16,7 +16,21 @@ let Tag = 'signin.js: ';
  * Setup Configuration file Requirements:
  */
 
+let vueRenderOptions = {
+    head: {
+        meta: [
+            {script: '/javascripts/ui_functions.js'},
+            {script: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js'},
+            {style: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-orange.min.css'},
+            {style: 'https://code.getmdl.io/1.3.0/material.indigo-orange.min.css'}
+
+        ]
+    }
+};
+
 router.get('/', function (req, res, next) {
-    res.render('./Desktop/signin', {title: 'NLA - Natural Language Analyse Tool'});
+    res.renderVue('signin', vueRenderOptions);
 });
 module.exports = router;
+
+
