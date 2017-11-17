26fabbc7c2123bc5ce347294b8a01e20b1cafdef
Florin <florin12@gmx.de>
2017-11-17 16:14:52 +0100

    migrated profile.ejs to vue

---
+++ views/profile.vue
@@ -0,0 +1,21 @@
+<template>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>{{ title }}</title>
+        <link rel='stylesheet' href='/css/style.css'/>
+    </head>
+    <body>
+    <h1>{{title }}</h1>
+    <p>Welcome to {{ title }}</p>
+    </body>
+    </html>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+            }
+        }
+    }
+</script>
