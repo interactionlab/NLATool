a1da66ea1bafee770a49b6a9c235eed934498adb
GustavMurawski <gustav.murawski@gmail.com>
2017-11-20 14:30:54 +0100

    Commentary-Function

---
+++ public/javascripts/comment.js
@@ -0,0 +1,51 @@
+var openid, uid;
+function parseInput() {
+    var text = document.getElementById('input').value;
+    var words = text.split(' ');
+    var html = '';
+    var url = '/comment/init';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            uid = JSON.parse(this.responseText).uid;
+            //document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhttp.send(JSON.stringify({"words": words}));
+    for (let i=0; i< words.length;i++) {
+        html += '<span style="padding: 0 5px;" onclick="addComment(' + i + ')" id="' + i + '">' + words[i] + '</span>';
+    }
+    document.getElementById('output').innerHTML = html;
+}
+function addComment(id) {
+    var url = "/comment/";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            //$('#modal').css('display', 'block');
+            let res = JSON.parse(this.responseText);
+            document.getElementById('modal').style.display = 'block';
+            //$('#word').html(words[id]);
+            document.getElementById('word').innerHTML = res.word;
+            document.getElementById('comment').value = res.comment;
+            openid = id;
+        }
+    };
+    xhttp.open("GET", url + "/" + id + "/?uid=" + uid, true);
+    xhttp.send();
+}
+function saveComment() {
+    let url = "/comment/save";
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            //document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhttp.send(JSON.stringify({"uid": uid, "id": openid, "comment": document.getElementById('comment').value}));
+    document.getElementById('modal').style.display = 'none';
+}
---
+++ views/comment.vue
@@ -0,0 +1,25 @@
+<template>
+<html>
+<head>
+    <script src="/javascripts/comment.js"></script>
+</head>
+<body>
+    <div id="modal" style="display:none;top:100px;width:500px;position:absolute;margin:auto;">
+      <h1 id="word"></h1>
+      <textarea id="comment"></textarea>
+      <input type="button" onclick="saveComment();">
+    </div>
+<textarea id="input"></textarea>
+<input type="button" value="parse" onclick="parseInput();">
+<div id="output"></div>
+</body>
+</html>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+            }
+        }
+    }
+</script>
