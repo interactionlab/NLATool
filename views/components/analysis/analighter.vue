eafd3b21a99ea42f396f58e11faa76916aa1e671
telion2 <telion.official1@hotmail.com>
2017-11-20 18:12:31 +0100

    work on the new components

--- views/components/analysis/analighter.vue
+++ views/components/analysis/analighter.vue
@@ -1,14 +1,25 @@
 <template>
-
-
+    <ul>
+        <li><button v-on:click="showNer">NER</button></li>
+        <li><button v-on:click="showNed">NED</button></li>
+        <li><button v-on:click="showNec">NEC</button></li>
+    </ul>
+    
 </template>
 <script>
     import ner from '../../mixins/analysis/ner';
+    import ned from '../../mixins/analysis/ned';
+    import nec from '../../mixins/analysis/nec';
+
+    import nerVue from './ner.vue';
     export default {
-        mixins: [ner],
+        mixins: [ner, ned, nec],
         data: function () {
             return{}
 
+        },
+        components:{
+            nerVue
         }
     }
 </script>
--- views/mixins/analysis/nec.js
+++ views/mixins/analysis/nec.js
@@ -1,4 +1,7 @@
 module.exports ={
     methods:{
+        showNec:function () {
+            
+        }
     }
 };
