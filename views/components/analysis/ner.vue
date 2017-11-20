54675ba7d11968bcdcb30f2291d56cc5f9373468
telion2 <telion.official1@hotmail.com>
2017-11-20 18:12:31 +0100

    work on the new components

---
+++ views/components/analysis/ner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>{{ curentWord }}</div>
+        <div>
+
+
+        </div>
+    </div>
+
+</template>
+<script>
+    export default {
+        data: function () {
+            return{}
+
+        }
+    }
+</script>
