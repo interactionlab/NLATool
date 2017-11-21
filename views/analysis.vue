8c704927e1f2e8c2ed1550aa7e29a20a9824e15e
telion2 <telion.official1@hotmail.com>
2017-11-21 13:17:39 +0100

    work on analysis.vue

--- views/analysis.vue
+++ views/analysis.vue
@@ -14,7 +14,7 @@
     <body>
     <!-- Uses a header that contracts as the page scrolls down. -->
     <style>
-        .demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type  {
+        .demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type {
             padding-right: 0;
         }
     </style>
@@ -78,66 +78,29 @@
 
                     <!--right grid for result stuff -->
                     <div class="mdl-cell mdl-cell--6-col graybox">
+                        <!-- Features -->
+                        <nav class="mdl-navigation">
+                            <button v-on:click="getAnalighter"
+                                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                                <b class="mdc-button">Analysis</b>
+                            </button>
+                            <button v-on:click="getNotes"
+                                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                                <b class="mdc-button">Notes</b>
+                            </button>
+                            <button v-on:click="analysisMode = 'research'"
+                                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                                <b class="mdc-button">Research</b>
+                            </button>
+                        </nav>
                         <!-- onclick for show the results-->
-                        <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%; text-align: right;">
+                        <!--
+                        <div class="mdl-cell mdl-cell&#45;&#45;6-col graybox" style="width: 98%; text-align: right;">
                             <input type="submit" id="switch" value="v" onclick="expand();"/>
-                        </div>
-
-                        <!-- result area is closed (default)-->
-                        <div class="mdl-cell mdl-cell--6-col graybox" style="display: none;" id="right-column">
-
-                            <!-- display the selected NER-->
-                            <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-                                <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-                                    NER (name, place, etc) <!--<input type="submit" value="NER (name, place, etc)" /> -->
-                                </div>
-                            </div>
-
-                            <!-- Buttons for modify the result-->
-                            <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-                                <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-
-                                    <!-- Features -->
-                                    <nav class="mdl-navigation">
-                                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                            <b class="mdc-button">Analysis</b>
-                                        </button>
-                                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                            <b class="mdc-button">Notes</b>
-                                        </button>
-                                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                            <b class="mdc-button">Research</b>
-                                        </button>
-                                    </nav>
-
-
-                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                        <b class="mdc-button">Information</b>
-                                    </button>
-                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                        <b class="mdc-button">Karte</b>
-                                    </button>
-                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                        <b class="mdc-button">Statistik</b>
-                                    </button>
-                                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                        <b class="mdc-button">Korrektur</b>
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-                                <form action="#">
-                                    <div class="mdl-textfield mdl-js-textfield" style="width: 100%">
-                        <textarea class="mdl-textfield__input" type="text" rows="5" id="textbox">
-                        </textarea>
-                                        <label class="mdl-textfield__label" for="textbox">
-                                            Results will be displayed here.
-                                        </label>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+                        </div>-->
+                        <component :is="analysisMode"></component>
+                        <component is="research"></component>
+                        <component is="analighter"></component>
                     </div>
                 </div>
             </div>
@@ -148,9 +111,31 @@
 
 </template>
 <script>
+    import research from './components/analysis/research.vue';
+    import analighter from './components/analysis/analighter.vue';
+
+    let analysisMode = 'research';
     export default {
         data: function () {
             return {}
+        },
+
+        getAnalighter: function () {
+            this.analysisMode = 'analighter'
+        },
+        getNotes: function () {
+            this.analysisMode = 'notes';
+        },
+        getResearch: function () {
+            this.analysisMode = 'research';
+        },
+        components: {
+            research,
+            analighter
+
+        },
+        props: {
+            analysisMode: analysisMode
         }
     }
 </script>
--- views/components/analysis/research.vue
+++ views/components/analysis/research.vue
@@ -1,10 +1,10 @@
 <template>
 
     <!--right grid for result stuff -->
-    <div class="mdl-cell mdl-cell--6-col graybox">
+
 
         <!-- result area is closed (default)-->
-        <div class="mdl-cell mdl-cell--6-col graybox" style="display: none;" id="right-column">
+        <div class="mdl-cell mdl-cell--12-col graybox" id="right-column">
 
             <!-- display the selected NER-->
             <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
@@ -16,26 +16,25 @@
             <!-- Buttons for modify the result-->
             <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
                 <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
-                    <button v-on:click="quickResearch" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    <button v-on:click="quickResearch"
+                            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
                         <b class="mdc-button">Information</b>
                     </button>
                     <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                        <b class="mdc-button">Karte</b>
+                        <b class="mdc-button">Map</b>
                     </button>
                     <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                        <b class="mdc-button">Statistik</b>
+                        <b class="mdc-button">Statistics</b>
                     </button>
                     <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                        <b class="mdc-button">Korrektur</b>
+                        <b class="mdc-button">Correction</b>
                     </button>
                 </div>
             </div>
 
             <div class="mdl-cell mdl-cell--6-col graybox" style="width: 98%">
                 <form action="#">
-                    <div class="mdl-textfield mdl-js-textfield" style="width: 100%">
-                        <textarea class="mdl-textfield__input" type="text" rows="5" id="textbox">
-                        </textarea>
+                    <div class="mdl-textfield mdl-js-textfield" style="width: 100%" id="textbox">
                         <label class="mdl-textfield__label" for="textbox">
                             Results will be displayed here.
                         </label>
@@ -43,15 +42,16 @@
                 </form>
             </div>
         </div>
-    </div>
+
 </template>
 <script>
-    import ner from '../../mixins/analysis/research';
+    import research from './mixins/analysis/research';
+
     export default {
-        mixins: ner,
+        mixins: [research],
         data: function () {
-            return{}
-            
+            return {}
+
         }
     }
 </script>
