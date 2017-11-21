cbd8d6f7e921987ea73a7fbe4f43a38edfaa25e5
telion2 <telion.official1@hotmail.com>
2017-11-21 13:17:39 +0100

    work on analysis.vue

--- views/components/analysis/analighter.vue
+++ views/components/analysis/analighter.vue
@@ -1,26 +1,50 @@
 <template>
-    <ul>
-        <li><button v-on:click="showNer">NER</button></li>
-        <li><button v-on:click="showNed">NED</button></li>
-        <li><button v-on:click="showNec">NEC</button></li>
-    </ul>
-    <div v-if="">
-        <nerVue/>
+    <div>
+        <ul>
+            <li>
+                <button v-on:click="showNer">NER</button>
+            </li>
+            <li>
+                <button v-on:click="showNed">NED</button>
+            </li>
+            <li>
+                <button v-on:click="showNec">NEC</button>
+            </li>
+        </ul>
+        <div v-if="showMode === 'NER'">
+            <nerVue></nerVue>
+        </div>
+        <div v-else-if="showMode === 'NED'">
+            <nedVue></nedVue>
+        </div>
+        <div v-else-if="showMode === 'NEC'">
+            <necVue></necVue>
+        </div>
     </div>
-
 </template>
 <script>
-    import ner from '../../mixins/analysis/ner';
-    import ned from '../../mixins/analysis/ned';
-    import nec from '../../mixins/analysis/nec';
+    import ner from './mixins/analysis/ner';
+    import ned from './mixins/analysis/ned';
+    import nec from './mixins/analysis/nec';
 
-    import nerVue from './ner.vue';
+    import nerVue from './components/analysis/ner.vue';
+
+    let showMode = 'NER';
     export default {
         mixins: [ner, ned, nec],
-        data: function () {
-            return{}
+        showNer: function () {
+            showMode = 'NER';
         },
-        components:{
+        showNed: function () {
+            showMode = 'NED';
+        },
+        showNec: function () {
+            showMode = 'NEC';
+        },
+        data: function () {
+            return {}
+        },
+        components: {
             nerVue
         }
     }
