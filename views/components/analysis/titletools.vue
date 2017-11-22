5bd3b9b99c7d012b8710daf0a698cc2bf722e21a
telion2 <telion.official1@hotmail.com>
2017-11-22 21:16:29 +0100

    work on ui

--- views/components/analysis/titletools.vue
+++ views/components/analysis/titletools.vue
@@ -7,15 +7,15 @@
             <!--Insert a Space here:-->
             <span>
             <nav class="mdl-navigation">
-                <button v-on:click="$emit('getAnalighter')"
+                <button v-on:click="emitanalighter"
                         class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
                     <b class="mdc-button">Analysis</b>
                 </button>
-                <button v-on:click="$emit('getNotes')"
+                <button v-on:click="emitnotes"
                         class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
                     <b class="mdc-button">Notes</b>
                 </button>
-                <button v-on:click="$emit('getResearch')"
+                <button v-on:click="emitresearch"
                         class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
                     <b class="mdc-button">Research</b>
                 </button>
@@ -29,6 +29,17 @@
         data: function () {
             return {}
 
+        },
+        methods:{
+            emitanalighter:function () {
+                this.$emit('emitanalighter');
+            },
+            emitnotes:function () {
+                this.$emit('emitnotes');
+            },
+            emitresearch:function () {
+                this.$emit('emitresearch');
+            }
         }
     }
 </script>
--- views/index.vue
+++ views/index.vue
@@ -6,34 +6,7 @@
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <component is="mainheader2" v-bind:title="title" ></component>
         <component is="headernavbar" v-bind:title_small="title_small"></component>
-        <!--<header class="mdl-layout__header">
-                <div class="mdl-layout__header-row">
-                    &lt;!&ndash; Add spacer, to align navigation to the right &ndash;&gt;
-                    <div class="mdl-layout-spacer"></div>
-                    &lt;!&ndash; Headerbutton (3 dots) on the right&ndash;&gt;
-                    <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button&#45;&#45;icon" id="headerbtn">
-                        <i class="material-icons">more_vert</i>
-                    </button>
-                    &lt;!&ndash; Some examples for headerbutton&ndash;&gt;
-                    <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu&#45;&#45;bottom-right" for="headerbtn">
-                        &lt;!&ndash;They could/should be linked&ndash;&gt;
-                        <li class="mdl-menu__item">About</li>
-                        <li class="mdl-menu__item">Contact</li>
-                        <li class="mdl-menu__item">Legal information</li>
-                    </ul>
-                </div>
-            </header>
-            &lt;!&ndash; Sidebar content&ndash;&gt;
-            <div class="mdl-layout__drawer">
-                <span class="mdl-layout-title">{{ title_small }}</span> &lt;!&ndash;TODO title to big or to long&ndash;&gt;
-                <nav class="mdl-navigation">
-                    <a class="mdl-navigation__link" href="signin">Sign in</a>
-                    <a class="mdl-navigation__link" href="signup">Sign up</a>
-                    <a class="mdl-navigation__link" href="">Link</a>
-                    <a class="mdl-navigation__link" href="">Link</a>
-                </nav>
-            </div>
--->
+
         <!-- page content-->
         <main class="mdl-grid graybox" style="text-align: center">
             <div style=" width: 100%; text-align: center">
