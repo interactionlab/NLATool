377bb35885de78c2e434961fc1d2edb16fd7f043
telion2 <telion.official1@hotmail.com>
2017-11-22 16:41:18 +0100

    work on header and titletools.vue

--- views/components/global/mainheader.vue
+++ views/components/global/mainheader.vue
@@ -1,13 +1,9 @@
 <template>
-    <div>
-
-        <header class="mdl-layout__header mdl-layout__header--waterfall">
-            <!-- Top row, always visible -->
+        <header class="mdl-layout__header">
             <div class="mdl-layout__header-row">
-                <!-- Title -->
+                <!-- Add spacer, to align navigation to the right -->
                 <span class="mdl-layout-title">{{title}}</span>
                 <div class="mdl-layout-spacer"></div>
-
                 <!-- Headerbutton (3 dots) on the right-->
                 <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="headerbtn">
                     <i class="material-icons">more_vert</i>
@@ -19,30 +15,16 @@
                     <li class="mdl-menu__item">Contact</li>
                     <li class="mdl-menu__item">Legal information</li>
                 </ul>
-
-            </div>
-            <!-- Bottom row, not visible on scroll -->
-            <div class="mdl-layout__header-row">
-                <div class="mdl-layout-spacer"></div>
             </div>
         </header>
-        <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">{{title_small}}</span>
-            <nav class="mdl-navigation">
-                <a class="mdl-navigation__link" href="">Home</a>
-                <a class="mdl-navigation__link" href="">Load a Text</a>
-                <a class="mdl-navigation__link" href="">Contact</a>
-            </nav>
-        </div>
-    </div>
+        <!-- Sidebar content-->
 </template>
 <script>
     export default {
-        props:['title', 'title_small'],
+        props: ['title'],
         data: function () {
             return {
                 title: this.title,
-                title_small: this.title_small
             }
         }
     }
