52b501fb61bb07b11055ba58910801ddf7c221e1
telion2 <telion.official1@hotmail.com>
2017-11-22 12:46:51 +0100

    added some .vue and work on analysis.vue

--- views/analysis.vue
+++ views/analysis.vue
@@ -1,57 +1,11 @@
 <template>
     <!DOCTYPE html>
     <html>
-    <!-- <head>
-         <meta charset="UTF-8">
-         <meta name="viewport" content="width = device-width, initial-scale = 1">
-         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-orange.min.css">
-         <link rel="stylesheet"
-               href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-orange.min.css">
-         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-         <script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
-     </head>-->
     <body>
-    <!-- Uses a header that contracts as the page scrolls down. -->
-    <style>
-        .demo-layout-waterfall .mdl-layout__header-row .mdl-navigation__link:last-of-type {
-            padding-right: 0;
-        }
-    </style>
     <div class="demo-layout-waterfall mdl-layout mdl-js-layout">
-        <header class="mdl-layout__header mdl-layout__header--waterfall">
-            <!-- Top row, always visible -->
-            <div class="mdl-layout__header-row">
-                <!-- Title -->
-                <span class="mdl-layout-title">{{title}}</span>
-                <div class="mdl-layout-spacer"></div>
+        <!-- Uses a mainHeader that contracts as the page scrolls down. -->
+        <component is="mainheader" v-bind:title="title" v-bind:title_small="this.title_small"></component>
 
-                <!-- Headerbutton (3 dots) on the right-->
-                <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="headerbtn">
-                    <i class="material-icons">more_vert</i>
-                </button>
-                <!-- Some examples for headerbutton-->
-                <ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="headerbtn">
-                    <!--They could/should be linked-->
-                    <li class="mdl-menu__item">About</li>
-                    <li class="mdl-menu__item">Contact</li>
-                    <li class="mdl-menu__item">Legal information</li>
-                </ul>
-
-            </div>
-            <!-- Bottom row, not visible on scroll -->
-            <div class="mdl-layout__header-row">
-                <div class="mdl-layout-spacer"></div>
-            </div>
-        </header>
-        <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">{{title_small}}</span>
-            <nav class="mdl-navigation">
-                <a class="mdl-navigation__link" href="">Home</a>
-                <a class="mdl-navigation__link" href="">Load a Text</a>
-                <a class="mdl-navigation__link" href="">Contact</a>
-            </nav>
-        </div>
         <main class="mdl-layout__content">
             <div class="page-content">
                 <div class="mdl-grid">
@@ -89,18 +43,11 @@
                                 <b class="mdc-button">Notes</b>
                             </button>
                             <button v-on:click="getResearch"
-                                     class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                                 <b class="mdc-button">Research</b>
-                             </button>
+                                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                                <b class="mdc-button">Research</b>
+                            </button>
                         </nav>
-                        <!-- onclick for show the results-->
-                        <!--
-                        <div class="mdl-cell mdl-cell&#45;&#45;6-col graybox" style="width: 98%; text-align: right;">
-                            <input type="submit" id="switch" value="v" onclick="expand();"/>
-                        </div>-->
                         <component :is="analysisMode"></component>
-                        <!--<component is="research"></component>
-                        <component is="analighter"></component>-->
                     </div>
                 </div>
             </div>
@@ -113,7 +60,7 @@
 <script>
     import research from './components/analysis/research.vue';
     import analighter from './components/analysis/analighter.vue';
-
+    import mainheader from './components/global/mainHeader.vue';
     //let analysisMode = 'research';
     export default {
         data: function () {
@@ -138,7 +85,8 @@
         },
         components: {
             research,
-            analighter
+            analighter,
+            mainheader
         }
     }
 </script>
