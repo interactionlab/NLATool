d79ce686068c93d157f35a5f72611bdc815ccc54
telion2 <telion.official1@hotmail.com>
2017-11-22 16:41:18 +0100

    work on header and titletools.vue

--- views/components/analysis/titletools.vue
+++ views/components/analysis/titletools.vue
@@ -1,10 +1,11 @@
 <template>
-    <div class="mdl-cell mdl-cell--12-col">
-        <span><h4>bla</h4></span>
-        <!--Button that should make textTitle editable and send changes to server to update DB -->
-        <span><button>Edit</button></span>
-        <!--Insert a Space here:-->
-        <span>
+    <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--12-col">
+            <span><h4>bla</h4></span>
+            <!--Button that should make textTitle editable and send changes to server to update DB -->
+            <span><button>Edit</button></span>
+            <!--Insert a Space here:-->
+            <span>
             <nav class="mdl-navigation">
                 <button v-on:click="$emit('getAnalighter')"
                         class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
@@ -20,8 +21,8 @@
                 </button>
             </nav>
         </span>
+        </div>
     </div>
-
 </template>
 <script>
     export default {
---
+++ views/components/global/headernavbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">{{ title_small }}</span> <!--TODO title to big or to long-->
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="signin">Sign in</a>
+            <a class="mdl-navigation__link" href="signup">Sign up</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+            <a class="mdl-navigation__link" href="">Link</a>
+        </nav>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['title_small'],
+        data: function () {
+            return {
+                title_small: this.title_small
+            }
+        }
+    }
+</script>
