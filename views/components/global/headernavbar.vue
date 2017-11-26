f617a9d367cf60aa5d56983df8ecc412d1b681a4
turcanee <emine@live.com.my>
2017-11-26 10:42:08 +0100

    added new options for sidebar if not logged in

--- views/components/global/headernavbar.vue
+++ views/components/global/headernavbar.vue
@@ -3,9 +3,8 @@
         <span class="mdl-layout-title">{{ title_small }}</span> <!--TODO title to big or to long-->
         <nav class="mdl-navigation">
             <a class="mdl-navigation__link" href="/">Home</a>
-            <a class="mdl-navigation__link" href="loadtext">Load a Text</a>
-            <a class="mdl-navigation__link" href="">Contact</a>
-            <a class="mdl-navigation__link" href="">Legal Information</a>
+            <a class="mdl-navigation__link" href="signin">Log in</a>
+            <a class="mdl-navigation__link" href="">Imprint</a>
         </nav>
     </div>
 </template>
