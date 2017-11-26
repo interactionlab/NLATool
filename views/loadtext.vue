c4d93dcec0cda3e8e94e5af6016b4e70f0189795
turcanee <emine@live.com.my>
2017-11-26 11:23:51 +0100

    centered ui elements and changed button style for analyze

--- views/loadtext.vue
+++ views/loadtext.vue
@@ -10,22 +10,24 @@
         <main class="mdl-layout__content">
 
             <form action="/loadtext/loadWrittenText" method="post">
+                <br/>
                 <!-- Textfield for title-->
-                <div class="mdl-textfield mdl-js-textfield graybox">
+                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--4-col graybox" style="text-align: left">
                     <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
                     <label class="mdl-textfield__label" for="titlebox">Title</label>
                 </div>
                 <!-- Textfield for textinput -->
-                <div class="mdl-textfield mdl-js-textfield graybox" style="width: 100%">
+                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col graybox" style="text-align: center">
                     <textarea class="mdl-textfield__input" type="text" rows="6" id="textbox" name="textInput">
                     </textarea>
                     <label class="mdl-textfield__label" for="textbox">
                         Enter text here ...
                     </label>
                 </div>
-                <div>
+                <!-- TODO add link to analysis use on-click -->
+                <div class="mdl-cell mdl-cell--12-col graybox " style="text-align: center">
                     <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                        <i class="material-icons mdl-navigation__link" href="analyse">send</i>
+                        <b class="mdc-button mdl-navigation__link" href="analysis">Analyze</b>
                     </button>
                 </div>
             </form>
