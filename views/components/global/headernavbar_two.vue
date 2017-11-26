2cb78c9166acb711919b0e73436b1cb57412ecdf
turcanee <emine@live.com.my>
2017-11-26 10:38:54 +0100

    added sidebar if logged in

---
+++ views/components/global/headernavbar_two.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">{{ title_small }}</span> <!--TODO title to big or to long-->
+        <nav class="mdl-navigation">
+            <a class="mdl-navigation__link" href="/">Home</a>
+        <!-- TODO additional options for New -->
+            <a class="mdl-navigation__link" href="loadtext">+ New</a>
+            <a class="mdl-navigation__link" href="">History</a>
+            <a class="mdl-navigation__link" href="">Contact</a>
+            <a class="mdl-navigation__link" href="">Imprint</a>
+        </nav>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['title_small'],
+        data: function () {
+            return {
+                title_small: this.title_small
+            }
+        }
+    }
+</script>
