8a7b518f098de7df3a2f5621838fff71d509246d
turcanee <emine@live.com.my>
2017-11-29 21:48:00 +0100

    can select words by index.Todo to vue

--- views/components/analysis/notes.vue
+++ views/components/analysis/notes.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <p>Notes come here!</p>
+        <input type="submit" id="commentt" value=" Comment " onclick="commentWord()"/>
+        <br />
+        <textarea id="textareaa"></textarea>
     </div>
 </template>
 <script>
@@ -8,6 +10,26 @@
         data: function () {
             return{}
 
-        }
+        },
+
+        let: interpunction = [".", ",", "\"", "'", "(", ")", "[", "]", "?", "!"],
+
+    function : commentWord() {
+        let txtarea = document.getElementById("textarea");
+        let start = txtarea.selectionStart;
+        let finish = txtarea.selectionEnd;
+        let sel = txtarea.value.substring(start, finish).trim();
+        let word = sel.split(' ')[0];
+
+        interpunction.forEach(function(sign) {
+            word = word.replace(sign, "");
+        });
+
+        let allWords = textarea.value.split(' ');
+        let wordCount = allWords.length;
+        let index = allWords.indexOf(word) + 1;
+
+        alert("Woerter insgesamt: " + wordCount + "\nWort Index: " + String(index) + "\nAusgewaehltes Wort: " + word);
+
     }
 </script>
