3d2339e047bd0663703b02dcc431fa5d87184573
GustavMurawski <gustav.murawski@gmail.com>
2017-12-01 00:05:20 +0100

    Refactoring

--- routes/comment.js
+++ routes/comment.js
@@ -1,18 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-
+let vueRenderOptions = {
+    head: {
+        meta: [
+            {script: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js'},
+            {script: '/javascripts/ui_functions.js'},
+            //{script: '/javascripts/comment.js'},
+            {script: 'https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.0/mark.js'},
+            {style: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-orange.min.css'},
+            {style: 'https://code.getmdl.io/1.3.0/material.indigo-orange.min.css'}
+        ]
+    }
+};
 
 router.get('/', function (req, res) {
-    res.renderVue('comment', {});
+    let uid = req.query.uid;
+    if (uid) {
+        res.renderVue('comment', {uid: uid, id: '', word: '', comment: '', words: req.session.wordTables[uid].words}, vueRenderOptions);
+    } else {
+        res.renderVue('comment', {uid: '', id: '', word: '', comment: '', words: []}, vueRenderOptions);
+    }
 });
 
 router.get('/:id', function (req, res) {
-    let uid = req.body.uid;
-    let id = req.body.id;
+    let uid = req.query.uid;
+    let id = req.params.id;
     let word = req.session.wordTables[uid].words[id];
     let comment = req.session.wordTables[uid].comments[id];
-    res.json({word: word, comment: comment});
+    res.renderVue('comment', {uid: uid, id: id, word: word, comment: comment, words: req.session.wordTables[uid].words}, vueRenderOptions);
 });
 
 
@@ -20,7 +36,8 @@ router.post('/save', function (req, res) {
     let uid = req.body.uid;
     let id = req.body.id;
     req.session.wordTables[uid].comments[id] = req.body.comment;
-    res.json({});
+    res.redirect('/comment?uid=' + uid);
+    //res.renderVue('comment', {uid: uid, id: '', word: '', comment: '', words: req.session.wordTables[uid].words}, vueRenderOptions);
 });
 
 function guid() {
@@ -35,17 +52,12 @@ function guid() {
 
 router.post('/init', function (req, res) {
     let uid = guid();
-    let words = req.body.words;
+    let words = req.body.text.split(' ');
     let comments = new Array(words.length);
     if (!req.session.wordTables) {
         req.session.wordTables = {};
     }
     req.session.wordTables[uid] = {words: words, comments: comments};
-    res.json({uuid: uuid});
-    res.render('./Desktop/comment', {
-        title: 'NLA - Natural Language Analyse Tool'
-        //result :
-    });
-
+    res.redirect('/comment?uid=' + uid);
 });
-    module.exports = router;
+module.exports = router;
