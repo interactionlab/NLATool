b347f02902bf058499bee21c7a4117a5f0dac931
telion2 <telion.official1@hotmail.com>
2017-11-30 16:42:23 +0100

    started implementing the main components for notes.vue

---
+++ views/components/analysis/notes/newwordnote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <a>Set a Link for word</a>
+        <textarea v-model="note"></textarea>
+        <button v-on:click="save"></button>
+        <button v-on:click="back"></button>
+    </div>
+</template>
+<script>
+    export default {
+        props:{
+            note: String,
+        },
+        data: function () {
+            return{
+                note: this.note
+            }
+        },
+        methods: {
+            back: function(){
+                this.$emit()
+            },
+            save: function(){
+                //TODO: send note to Server -> DB
+                //TODO: change Component to wordnotes
+            }
+        }
+    }
+</script>
