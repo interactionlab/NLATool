ec36e2c29e1fc1bd68c837b2bfbdd61973e87fc0
telion2 <telion.official1@hotmail.com>
2017-11-30 15:45:14 +0100

    first steps for notes.vue

---
+++ views/components/analysis/notes/wordnote.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <a>Link of word</a>
+        <p>{{ wordnote }}</p>
+        <button v-on:click="edit">Editicon</button>
+        <button v-on:click="deleting">Deleteicon</button>
+    </div>
+</template>
+<script>
+    export default {
+        props:{
+            wordnote: String
+        },
+        data: function () {
+            return{
+                wordnote: this.wordnote
+            }
+        },
+        methods: {
+            edit: function(){
+
+            },
+            deleting: function () {
+                //1. delete entry in db
+                //2. delete current component
+            }
+        }
+    }
+</script>
