b6e5c2d16dc946517657ddb9486fd3a658305fe6
LuxyNerd <emine@live.com.my>
2017-12-04 19:11:39 +0100

    notestoolbar added

---
+++ views/components/analysis/toolbar/notestool.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="mdl-cell mdl-cell--12-col graybox">
+        <div>
+            <!--Switch between word notes and text notes-->
+        </div>
+        <component :is="noteMode"></component>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "notestool"
+    }
+</script>
+
+<style scoped>
+
+</style>
