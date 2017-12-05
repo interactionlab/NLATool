445e23f0b78e8f30d4e1b2904c176ab3ac55bb93
LuxyNerd <emine@live.com.my>
2017-12-05 18:40:58 +0100

    removed unuseful Buttons for resarch results

--- views/components/analysis/research.vue
+++ views/components/analysis/research.vue
@@ -4,22 +4,6 @@
         <div class="mdl-cell mdl-cell--12-col graybox">
             NER (name, place, etc) <!--<input type="submit" value="NER (name, place, etc)" /> -->
         </div>
-        <!-- Buttons for modify the result-->
-        <div class="mdl-cell mdl-cell--12-col graybox">
-            <button v-on:click="quickResearch"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                <b class="mdc-button">Information</b>
-            </button>
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                <b class="mdc-button">Map</b>
-            </button>
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                <b class="mdc-button">Statistics</b>
-            </button>
-            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                <b class="mdc-button">Correction</b>
-            </button>
-        </div>
         <div class="mdl-cell mdl-cell--12-col graybox">
             <form action="#">
                 <div class="mdl-textfield mdl-js-textfield" id="textbox">
