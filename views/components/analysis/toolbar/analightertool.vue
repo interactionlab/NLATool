45793e5b6dfbfc12feb53987dd10bfdf2009f9f2
Florin <florin12@gmx.de>
2017-12-06 15:34:35 +0100

    added functionality for other sem classes

--- views/components/analysis/toolbar/analightertool.vue
+++ views/components/analysis/toolbar/analightertool.vue
@@ -1,18 +1,37 @@
-
 <template>
     <div class="graybox">
         <button v-on:click="showNer"
                 class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-            <b class="mdc-button">NER</b>
+            <b class="mdc-button">NE</b>
         </button>
         <button v-on:click="showNed"
                 class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-            <b class="mdc-button">NED</b>
+            <b class="mdc-button">FM</b>
         </button>
         <button v-on:click="showNec"
                 class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-            <b class="mdc-button">NEC</b>
+            <b class="mdc-button">NN</b>
         </button>
+        <button v-on:click="showPersons"
+                class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            <b class="mdc-button">PERSONS</b>
+        </button>
+        <button v-on:click="showLoc"
+                class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            <b class="mdc-button">LOCATION</b>
+        </button>
+        <button v-on:click="showOrg"
+                         class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+        <b class="mdc-button">ORGANIZATION</b>
+        </button>
+        <button v-on:click="showMisc"
+                class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+            <b class="mdc-button">MISC</b>
+        </button>
+        <button>
+
+        </button>
+
     </div>
 </template>
 
@@ -33,6 +52,22 @@
                 this.showMode = 'necVue';
                 this.$emit('changemarkermode', ['NN']);
             },
+            showPersons: function () {
+                this.showMode = 'nerVue';
+                this.$emit('changemarkermode', ['I-PER']);
+            },
+            showLoc: function () {
+                this.showMode = 'nerVue';
+                this.$emit('changemarkermode', ['I-LOC']);
+            },
+            showOrg: function () {
+                this.showMode = 'nerVue';
+                this.$emit('changemarkermode', ['I-ORG']);
+            },
+            showMisc: function () {
+                this.showMode = 'nerVue';
+                this.$emit('changemarkermode', ['I-MISC']);
+            }
         },
     }
 </script>
