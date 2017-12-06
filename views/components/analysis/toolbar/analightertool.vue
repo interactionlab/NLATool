78f77eb08f218e837357a8805a0f4e2e4831d1c7
LuxyNerd <emine@live.com.my>
2017-12-06 22:03:06 +0100

    toggle function do not work

--- views/components/analysis/toolbar/analightertool.vue
+++ views/components/analysis/toolbar/analightertool.vue
@@ -1,6 +1,13 @@
 
 <template>
     <div class="graybox">
+        <label v-on:click="showNer"
+                class = "mdl-icon-toggle mdl-js-icon-toggle mdl-js-ripple-effect"
+               for = "icon-toggle-1">
+            <input type = "checkbox" id = "icon-toggle-1"
+                   class = "mdl-icon-toggle__input" checked>
+            <i class = "mdl-icon-toggle__label">NER</i>
+        </label>
         <button v-on:click="showNer"
                 class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
             <b class="mdc-button">NER</b>
@@ -13,7 +20,6 @@
                 class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent ">
             <b class="mdc-button ">NEC</b>
         </button>
-
     </div>
 </template>
 
