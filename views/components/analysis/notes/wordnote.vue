b6d16a6cd5d439b47f461e2b79cc62007337906f
LuxyNerd <emine@live.com.my>
2017-12-07 11:48:55 +0100

    TODO done and delete button added

--- views/components/analysis/notes/wordnote.vue
+++ views/components/analysis/notes/wordnote.vue
@@ -2,8 +2,10 @@
     <div class="mdl-grid" style="width: 100%">
         <a>Link of word</a>
         <p class="mdl-cell mdl-cell--12-col graybox" v-on:click="edit" >{{ wordnote }}</p>
-        <!--TODO:@Emmi Replace with Icons-->
-        <button class="mdl-button" v-on:click="deleting">Deleteicon</button>
+
+        <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" v-on:click="deleting">
+            <i class="material-icons">delete</i>
+        </button>
     </div>
 </template>
 <script>
