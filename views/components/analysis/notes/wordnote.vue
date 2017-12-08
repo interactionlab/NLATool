67def8c6f365881a065124daadf2c2075d677c82
telion2 <telion.official1@hotmail.com>
2017-12-08 12:19:59 +0100

    load notes from db

--- views/components/analysis/notes/wordnote.vue
+++ views/components/analysis/notes/wordnote.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mdl-grid" style="width: 100%">
         <p>{{clickedword}}</p>
-        <p class="mdl-cell mdl-cell--12-col graybox" v-on:click="edit" >{{ wordnote }}</p>
+        <p class="mdl-cell mdl-cell--12-col graybox" v-on:click="edit" >{{ wordnotedb.content }}</p>
         <!--TODO:@Emmi Replace with Icons-->
         <button class="mdl-button" v-on:click="deleting">Deleteicon</button>
     </div>
@@ -9,16 +9,16 @@
 <script>
     export default {
         props:{
-            wordnote: String
+            wordnotedb: String
         },
         data: function () {
             return{
-                wordnote: this.wordnote
+                wordnotedb: this.wordnotedb
             }
         },
         methods: {
             edit: function(){
-                this.$emit('edit', [this.wordnote]);
+                this.$emit('edit', [this.wordnotedb]);
             },
             deleting: function () {
                 //1. delete entry in db
