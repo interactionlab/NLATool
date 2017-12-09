81e1986d1a1d51366e2e788eae0fc82f10ac3160
LuxyNerd <emine@live.com.my>
2017-12-09 23:26:42 +0100

    grids added for uniform look

--- views/loadtext.vue
+++ views/loadtext.vue
@@ -5,37 +5,41 @@
         <component is="headernavbar" v-bind:title_small="title_small"></component>
 
         <main class="mdl-layout__content">
-
             <form action="/loadWrittenText" method="post">
-                <!-- Textfield for title-->
-                <div class="mdl-grid mdl-cell--8-col contentColor">
-                    <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--4-col contentColor">
-                        <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
-                        <label class="mdl-textfield__label" for="titlebox">Title</label>
+                <div class="mdl-grid">
+                    <!-- Textfield for title-->
+                    <div class="mdl-grid mdl-cell--8-col contentColor">
+                        <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--4-col contentColor">
+                            <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
+                            <label class="mdl-textfield__label" for="titlebox">Title</label>
+                        </div>
                     </div>
                 </div>
                 <!-- Textfield for textinput -->
-                <div class="mdl-grid mdl-cell--8-col contentColor">
-                    <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--8-col contentColor"
-                         style="width: 100%" >
+                <div class="mdl-grid">
+                    <div class="mdl-grid mdl-cell--8-col contentColor">
+                        <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--8-col contentColor"
+                             style="width: 100%">
                     <textarea class="mdl-textfield__input" type="text" rows="10" id="textbox" name="textInput">
                     </textarea>
-                        <label class="mdl-textfield__label" for="textbox">
-                            Enter text here ...
-                        </label>
+                            <label class="mdl-textfield__label" for="textbox">
+                                Enter text here ...
+                            </label>
+                        </div>
                     </div>
                 </div>
                 <!-- Analyze Button -->
-                <div class="mdl-grid mdl-cell--8-col contentColor">
-                    <div class="mdl-grid contentColor" style="text-align: center"> <!-- extra div needed to make button in center -->
-                        <button class="mdl-button mdl-js-button">
-                            <b class="mdc-button">Analyze</b>
-                        </button>
+                <div class="mdl-grid">
+                    <div class="mdl-grid mdl-cell--8-col contentColor">
+                        <div class="mdl-grid contentColor" style="text-align: center">
+                            <!-- extra div needed to make button in center -->
+                            <button class="mdl-button mdl-js-button">
+                                <b class="mdc-button">Analyze</b>
+                            </button>
+                        </div>
                     </div>
                 </div>
-
             </form>
-
         </main>
     </div>
 </template>
