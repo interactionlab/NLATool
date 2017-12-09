bc164a425b56cf84a98c54a86cfb9351c9845b14
LuxyNerd <emine@live.com.my>
2017-12-09 12:31:43 +0100

    button formate changed pls give me feed because can not test

--- views/components/analysis/toolbar/analightertool.vue
+++ views/components/analysis/toolbar/analightertool.vue
@@ -2,31 +2,31 @@
     <div>
         <div class="graybox">
             <button v-on:click="showNer"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">ALL</b>
             </button>
             <button v-on:click="showPersons"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">PERSONS</b>
             </button>
             <button v-on:click="showLoc"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">LOCATION</b>
             </button>
             <button v-on:click="showOrg"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">ORGANIZATION</b>
             </button>
             <button v-on:click="showMisc"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">MISC</b>
             </button>
             <button v-on:click="showNed"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button">
                 <b class="mdc-button">(FM)</b>
             </button>
             <button v-on:click="showNec"
-                    class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                    class="mdl-button mdl-js-button ">
                 <b class="mdc-button">(NN)</b>
             </button>
         </div>
