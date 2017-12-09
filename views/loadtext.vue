1509c3843b89f3c588c6651955ebce0cc46b6757
LuxyNerd <emine@live.com.my>
2017-12-09 11:56:40 +0100

    modified contents in startpagead

--- views/loadtext.vue
+++ views/loadtext.vue
@@ -1,40 +1,43 @@
 <template>
-    <!DOCTYPE html>
-    <html>
-    <body>
-
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <!-- Uses a mainHeader that contracts as the page scrolls down. -->
         <component is="mainheader" v-bind:title="title"></component>
         <component is="headernavbar" v-bind:title_small="title_small"></component>
+
         <main class="mdl-layout__content">
 
             <form action="/loadWrittenText" method="post">
-                <br/>
                 <!-- Textfield for title-->
-                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--4-col graybox" style="text-align: left">
-                    <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
-                    <label class="mdl-textfield__label" for="titlebox">Title</label>
+                <div class="mdl-grid mdl-cell--8-col graybox">
+                    <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--4-col graybox">
+                        <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
+                        <label class="mdl-textfield__label" for="titlebox">Title</label>
+                    </div>
                 </div>
                 <!-- Textfield for textinput -->
-                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col graybox" style="text-align: center">
-                    <textarea class="mdl-textfield__input" type="text" rows="6" id="textbox" name="textInput">
+                <div class="mdl-grid mdl-cell--8-col graybox">
+                    <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--8-col graybox"
+                         style="width: 100%" >
+                    <textarea class="mdl-textfield__input" type="text" rows="10" id="textbox" name="textInput">
                     </textarea>
-                    <label class="mdl-textfield__label" for="textbox">
-                        Enter text here ...
-                    </label>
+                        <label class="mdl-textfield__label" for="textbox">
+                            Enter text here ...
+                        </label>
+                    </div>
+                </div>
+                <!-- Analyze Button -->
+                <div class="mdl-grid mdl-cell--8-col graybox">
+                    <div class="mdl-grid graybox" style="text-align: center"> <!-- extra div needed to make button in center -->
+                        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                            <b class="mdc-button">Analyze</b>
+                        </button>
+                    </div>
                 </div>
 
-                <div class="mdl-cell mdl-cell--12-col graybox " style="text-align: center">
-                    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                        <b class="mdc-button">Analyze</b>
-                    </button>
-                </div>
             </form>
+
         </main>
     </div>
-    </body>
-    </html>
 </template>
 <script>
     import headernavbar from './components/global/headernavbar.vue';
