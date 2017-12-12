02e2d8a3126a6abb6d5a7349ffbd72d9ece5c1a2
telion2 <telion.official1@hotmail.com>
2017-12-12 13:43:42 +0100

    added ui components to signin

---
+++ views/components/userauthentification/login.vue
@@ -0,0 +1,19 @@
+<template>
+    <form action="/signin/login" method="post">
+        Benutzername oder Email<br>
+        <input type="text" name="user"><br>
+        Password<br>
+        <input type="text" name="pass"><br>
+        <input type="submit">
+    </form>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>
