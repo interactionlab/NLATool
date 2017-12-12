e2075721d6fcb6411144939323c045d008586c86
LuxyNerd <emine@live.com.my>
2017-12-12 22:43:52 +0100

    Button placeholder added and toolbar size should not change

--- views/components/analysis/toolbar/toolbar.vue
+++ views/components/analysis/toolbar/toolbar.vue
@@ -5,60 +5,65 @@
                 <!--Main Navigation:-->
                 <div class="mdl-tabs__tab-bar">
                     <a href="#analysis-panel" class="mdl-tabs__tab is-active" v-on:click="changetool('analightertool')">Analysis</a>
-                    <a href="#notes-panel" class="mdl-tabs__tab" v-on:click="changetool('notestool')">Notes</a>
                     <a href="#research-panel" class="mdl-tabs__tab" v-on:click="changetool('researchtool')">Research</a>
+                    <a href="#notes-panel" class="mdl-tabs__tab" v-on:click="changetool('notestool')">Notes</a>
                 </div>
             </div>
             <!--Tab values-->
             <div class="mdl-grid">
-            <div class="mdl-tabs__panel is-active " id="analysis-panel">
-                <button v-on:click="changeMarkerMode('NE')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">ALL</small>
-                </button>
-                <button v-on:click="changeMarkerMode('Person')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">PERSONS</small>
-                </button>
-                <button v-on:click="changeMarkerMode('Location')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">LOCATION</small>
-                </button>
-                <button v-on:click="changeMarkerMode('Organization')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">ORGANIZATION</small>
-                </button>
-                <button v-on:click="changeMarkerMode('Misc')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">MISC</small>
-                </button>
-                <button v-on:click="changeMarkerMode('FM')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">(FM)</small>
-                </button>
-                <button v-on:click="changeMarkerMode('NN')"
-                        class="mdl-button mdl-js-button">
-                    <small class="mdc-button">(NN)</small>
-                </button>
-            </div>
-            <div class="mdl-tabs__panel " id="notes-panel">
+                <div class="mdl-tabs__panel is-active " id="analysis-panel">
+                    <button v-on:click="changeMarkerMode('NE')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">ALL</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('Person')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">PERSONS</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('Location')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">LOCATION</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('Organization')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">ORGANIZATION</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('Misc')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">MISC</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('FM')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">(FM)</small>
+                    </button>
+                    <button v-on:click="changeMarkerMode('NN')"
+                            class="mdl-button mdl-js-button">
+                        <small class="mdc-button">(NN)</small>
+                    </button>
+                </div>
 
-            </div>
 
-            <div class="mdl-tabs__panel " id="research-panel">
-                <button class="mdl-button mdl-js-button">
-                    <small class="mdc-button">Information</small>
-                </button>
-                <button class="mdl-button mdl-js-button">
-                    <small class="mdc-button">Map</small>
-                </button>
-                <button class="mdl-button mdl-js-button">
-                    <small class="mdc-button">Statistics</small>
-                </button>
-                <button class="mdl-button mdl-js-button">
-                    <small class="mdc-button">Correction</small>
-                </button>
-            </div>
+                <div class="mdl-tabs__panel " id="research-panel">
+                    <button class="mdl-button mdl-js-button">
+                        <small class="mdc-button">Information</small>
+                    </button>
+                    <button class="mdl-button mdl-js-button">
+                        <small class="mdc-button">Map</small>
+                    </button>
+                    <button class="mdl-button mdl-js-button">
+                        <small class="mdc-button">Statistics</small>
+                    </button>
+                    <button class="mdl-button mdl-js-button">
+                        <small class="mdc-button">Correction</small>
+                    </button>
+                </div>
+
+                <div class="mdl-tabs__panel " id="notes-panel">
+                    <!--No Tab Value needed-->
+                    <button class="mdl-button mdl-js-button">
+                        <small class="mdc-button"></small>
+                    </button>
+                </div>
             </div>
         </div>
     </main>
