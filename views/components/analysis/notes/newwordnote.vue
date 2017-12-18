adbd97c493064e61c8262d0f7c78880a0b26fd27
telion2 <telion.official1@hotmail.com>
2017-12-18 10:38:37 +0100

    word on notes.vue

--- views/components/analysis/notes/newwordnote.vue
+++ views/components/analysis/notes/newwordnote.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-        <p>{{clickedword.word}}</p>
-        <div class="mdl-grid">
-            <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col">
-                <textarea class="mdl-cell mdl-cell--12-col mdl-textfield__input contentColor"
-                          style="width: 100%"
+    <div class="mdl-grid">
+        <p class="mdl-cell mdl-cell--12-col">{{clickedword.word}}</p>
+        <form class="mdl-cell mdl-cell--8-col">
+            <div class="mdl-textfield mdl-js-textfield">
+                <textarea class="mdl-textfield__input contentColor"
                           v-model="newnote"
+                          v-on:keyup.enter="save"
                           id="textbox"
                           type="text"
                           rows="1">
@@ -14,22 +14,19 @@
                     New+
                 </label>
             </div>
-        </div>
-        <div class="mdl-grid">
-            <div class="mdl-layout-spacer"></div>
-            <div class="mdl-cell mdl-cell--4-col">
-                <button class="mdl-button "
-                        v-on:click="save">Save
-                </button>
-                <button class="mdl-button "
-                        v-on:click="back">Back
-                </button>
-                <button class="mdl-button  mdl-js-button mdl-js-ripple-effect mdl-button--icon"
-                        v-on:click="deleting"
-                        id="noteMenu">
-                    <i class="material-icons">delete</i>
-                </button>
-            </div>
+        </form>
+        <div class="mdl-cell mdl-cell--4-col">
+            <button class="mdl-button "
+                    v-on:click="save">Save
+            </button>
+            <button class="mdl-button "
+                    v-on:click="back">Back
+            </button>
+            <button class="mdl-button  mdl-js-button mdl-js-ripple-effect mdl-button--icon"
+                    v-on:click="deleting"
+                    id="noteMenu">
+                <i class="material-icons">delete</i>
+            </button>
         </div>
     </div>
 </template>
@@ -39,14 +36,14 @@
             clickedword: String,
             docid: String,
             newnote: String,
-            note: Object
+            wordnotedb: Object
         },
         data: function () {
             return {
                 clickedword: this.clickedword,
                 docid: this.docid,
                 newnote: this.newnote,
-                note: this.note
+                wordnotedb: this.wordnotedb
             }
         },
         methods: {
@@ -56,16 +53,28 @@
             save: function () {
                 console.log('DOCID: ' + this.docid + ' : ' + this.clickedword.wordID);
                 let socket = io('http://localhost:8080');
-
-                socket.emit('savewordnote', this.newnote, this.clickedword.wordID, this.docid);
-                this.$emit('back');
+                if (typeof this.wordnotedb === 'undefined') {
+                    socket.emit('savewordnote', this.newnote, this.clickedword.wordID, this.docid);
+                    let tempNote = {
+                        docID: this.docid,
+                        noteID: -1,
+                        wordID: -1,
+                        content: this.newnote,
+                        word: 'Temporaly not possible'
+                    };
+                    this.newnote = '';
+                    this.clickedword = {};
+                    this.$emit('back', -1, 1,tempNote);
+                } else {
+                    socket.emit('updatewordnote', this.wordnotedb.noteID, this.newnote);
+                    this.$emit('back', this.wordnotedb.noteID, 2, this.wordnotedb);
+                }
             },
             deleting: function () {
                 console.log('DOCID: ' + this.docid);
                 let socket = io('http://localhost:8080');
-
-                socket.emit('deletenote', this.note, this.clickedword.wordID, this.docid);
-                this.$emit('back');
+                socket.emit('deletenote', this.wordnotedb.noteID, this.clickedword.wordID, this.docid);
+                this.$emit('back', this.wordnotedb.nodeID, 0);
             }
         }
     }
