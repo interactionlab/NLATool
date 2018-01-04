ffcec69f3b02701d72aaf9f9691745c353ca9a0f
telion2 <telion.official1@hotmail.com>
2018-01-04 17:16:09 +0100

    frist steps for the profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,26 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 //--------------------------------------------------------
 /**
  * Tags for console Errors:
  * @type {string}
  */
-var desktop = 'desktop Version: ';
-var mobile = 'Mobile Version: ';
-var bigDesktop = 'Big Desktop Version: ';
-var notMedia = 'Not Media-Related Part: ';
-var Tag = 'profile.js: ';
+const desktop = 'desktop Version: ';
+const mobile = 'Mobile Version: ';
+const bigDesktop = 'Big Desktop Version: ';
+const notMedia = 'Not Media-Related Part: ';
+const Tag = 'profile.js: ';
 //--------------------------------------------------------
 /**
  * Setup Configuration file Requirements:
  */
-
+const dbStub = require('../modules/db_stub');
+const dbAction = require('../modules/db_actions');
+const wait = require('wait.for');
 
 let vueRenderOptions = {
     head: {
         meta: [
-
             {script: '/javascripts/data_management.js'},
             {style: 'https://code.getmdl.io/1.3.0/material.indigo-blue.min.css'},
             {style: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-green.min.css'}
@@ -28,9 +29,17 @@ let vueRenderOptions = {
     }
 };
 
+/**
+ * Vue data object to be set for this route.
+ * @type {{vueText: null, vueTokens: null}}
+ */
+let vueData = {
+
+};
 
 router.get('/', function (req, res, next) {
-    res.renderVue('profile',vueRenderOptions);
+
+    res.renderVue('profile',vueData, vueRenderOptions);
 });
 
 module.exports = router;
