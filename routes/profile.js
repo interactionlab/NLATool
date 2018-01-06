8a06a19bb50ef0c6fe51f0c2257201facf2d4917
telion2 <telion.official1@hotmail.com>
2018-01-06 21:21:37 +0100

    major elements of the profile implements

--- routes/profile.js
+++ routes/profile.js
@@ -18,28 +18,140 @@ const Tag = 'profile.js: ';
 const dbStub = require('../modules/db_stub');
 const dbAction = require('../modules/db_actions');
 const wait = require('wait.for');
+const io = require('socket.io')(8081);
 
+/**
+ * Object that holds all specific meta info for this route.
+ * @type {{head: {meta: [null,null,null,null]}}}
+ */
 let vueRenderOptions = {
     head: {
         meta: [
-            {script: '/javascripts/data_management.js'},
-            {style: 'https://code.getmdl.io/1.3.0/material.indigo-blue.min.css'},
-            {style: 'https://storage.googleapis.com/code.getmdl.io/1.0.6/material.indigo-green.min.css'}
+            {script: 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.4/socket.io.js'},
         ]
     }
 };
 
 /**
  * Vue data object to be set for this route.
- * @type {{vueText: null, vueTokens: null}}
+ * @type {{documents: null}}
  */
 let vueData = {
-
+    documents: null,
+    amountOfDocuments: null,
+    amountOfButtons: null
+};
+/**
+ * locally global Variable package.
+ * @type {{documents: null}}
+ */
+let profileData = {
+    documents: null,
+    amountOfDocuments: null,
+    amountOfButtons: null,
+    generalAmount: 50
 };
 
-router.get('/', function (req, res, next) {
+/**
+ * Socket.io section for changing titles of documents and
+ * getting other specified documents.
+ */
+io.on('connection', function (socket) {
+    socket.on('changeTitle', function (docID, newTitle) {
+        console.log(notMedia + Tag + 'update Document Title: ');
+        wait.launchFiber(updateTitle, docID, newTitle);
+    });
+    socket.on('deleteDocument', function (docID) {
 
-    res.renderVue('profile',vueData, vueRenderOptions);
+    });
 });
+
+router.get('/', function (req, res, next) {
+    wait.launchFiber(prepareProfile, req, res, next);
+});
+
+router.post('/loadMoreDocuments', function (req, res, next) {
+    wait.launchFiber(loadMoreDocuments, req, res, next);
+});
+
+router.post('/loadDocument', function (req, res, next) {
+    req.session.docID = req.body.docID;
+    console.log(JSON.stringify(req.body));
+    res.redirect('/analysis');
+});
+
+/**
+ * replaces router.get with a fiber function.
+ * It retrieves the first 50 documents from the DB and sends it to the client
+ * while rendering the client page.
+ * @param req
+ * @param res
+ * @param next
+ */
+function prepareProfile(req, res, next) {
+    dbStub.nonFiberEstablishConnection();
+    profileData.amountOfDocuments = JSON.parse(wait.for(dbStub.makeSQLRequest, 'SELECT COUNT(*) FROM documents'));
+    profileData.amountOfDocuments = profileData.amountOfDocuments[0]['COUNT(*)'];
+
+    profileData.amountOfButtons = Math.trunc(profileData.amountOfDocuments / profileData.generalAmount);
+    if (profileData.amountOfDocuments % profileData.generalAmount > 0) {
+        profileData.amountOfButtons++;
+    }
+    if (typeof req.session.numberOfButton !== 'undefined') {
+        getDocuments(req.session.numberOfButton * profileData.generalAmount, profileData.generalAmount);
+    } else {
+        getDocuments(0, profileData.generalAmount);
+    }
+    vueData.documents = profileData.documents;
+    vueData.amountOfDocuments = profileData.amountOfDocuments;
+    vueData.amountOfButtons = profileData.amountOfButtons;
+    res.renderVue('profile', vueData, vueRenderOptions);
+}
+
+/**
+ * Replaces router.post to retrieve documents in another
+ * specified range.
+ * @param req
+ * @param res
+ * @param next
+ */
+function loadMoreDocuments(req, res, next) {
+    console.log(notMedia + Tag + 'load More Documents: ' + JSON.stringify(req.body.numberOfButton));
+    req.session.numberOfButton = req.body.numberOfButton-1;
+    res.redirect('/profile');
+}
+
+/**
+ * Gets and returns the Documentinfo in a specified range.
+ * @param start
+ * @param amount
+ */
+function getDocuments(start, amount) {
+    profileData.documents = JSON.parse(wait.for(
+        dbStub.makeSQLRequest,
+        dbAction.createLimitedSelectCommand(
+            'documents',
+            ['docID', 'userID', 'name'],
+            start, amount
+        )
+    ));
+    for(let i = 0;i < profileData.documents.length; i++){
+        if(profileData.documents[i].name.length === 0){
+            profileData.documents[i].name = 'No Name specified';
+        }
+    }
+    console.log(notMedia + Tag + 'documents: '+ JSON.stringify(profileData.documents));
+}
+
+/**
+ * Changes the title of a document on the DB.
+ * @param docID
+ * @param newTitle
+ */
+function updateTitle(docID, newTitle) {
+    docID = dbAction.stringifyForDB(docID);
+    newTitle = dbAction.stringifyForDB(newTitle);
+    wait.for(dbStub.makeSQLRequest, dbAction.createUpdateCommand('documents', ['name'], [newTitle], ['docID'], [docID], ['=']));
+}
 
 module.exports = router;
--- views/profile.vue
+++ views/profile.vue
@@ -5,8 +5,23 @@
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <!-- Uses a mainHeader that contracts as the page scrolls down. -->
         <component is="mainheader" v-bind:title="title"></component>
-        <component is="headernavbar" v-bind:title_small="title_small"></component>
+        <component is="headernavbar" v-bind:title_small="title_small"
+        ></component>
         <main class="mdl-layout__content">
+            <form action="/profile/loadMoreDocuments"
+                  method="post"
+                  v-for="numberOfButton in amountOfButtons"
+                  class="mdl-cell--12-col">
+                <button style="float: left" name="numberOfButton" v-bind:value="numberOfButton">{{numberOfButton}}</button>
+            </form>
+            <div class="mdl-layout-spacer"></div>
+            <component is="document"
+                       v-for="document in documents"
+                       v-bind:document="document"
+                       v-bind:key="document.docID"
+                       class="mdl-cell--8-col"
+            ></component>
+            <div class="mdl-layout-spacer"></div>
         </main>
     </div>
     </body>
@@ -15,14 +30,17 @@
 <script>
     import headernavbar from './components/global/headernavbar.vue';
     import mainheader from './components/global/mainheader.vue';
+    import document from './components/profile/document.vue';
+
     export default {
         data: function () {
-            return {
-            }
+            return {}
         },
-        components:{
+        components: {
             mainheader,
-            headernavbar
-        }
+            headernavbar,
+            document
+        },
+        computed: {},
     }
 </script>
