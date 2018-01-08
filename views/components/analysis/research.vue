3c9621530db9cdc58a7d0d135d9080575a14c6a8
LuxyNerd <emine@live.com.my>
2018-01-08 14:41:26 +0100

    rebuild query and used component

--- views/components/analysis/research.vue
+++ views/components/analysis/research.vue
@@ -10,7 +10,12 @@
                 <!--Results will be displayed here. -->
                 <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col graybox" id="resultfield">
                     <!-- {{searchGoogle.clickedword}} -->
-                    <component is = "results" v-for="researchResult in researchResults" v-bind:researchResult="researchResult"></component>
+                    <component is="researchresult"
+                               v-for="(researchresult,index) in researchresults.itemListElement"
+                               v-bind:researchresult="researchresult"
+                               v-bind:key="index"
+                               v-bind:research="researchresults"
+                    ></component>
                 </div>
             </form>
         </div>
@@ -18,10 +23,9 @@
 </template>
 
 <script>
-    import research from './mixins/analysis/research';
+    import researchresult from './components/analysis/researchresult.vue';
 
     export default {
-        mixins: [research],
         props: {
             clickedword: Object,
             researchmode: String
@@ -29,7 +33,7 @@
         data: function () {
             return {
                 clickedword: this.clickedword,
-                researchResults: 'Results will be displayed here.',
+                researchresults: [],
                 researchmode: this.researchmode
             }
         },
@@ -38,35 +42,42 @@
                 let service_url = 'https://kgsearch.googleapis.com/v1/entities:search';
                 let params = {
                     'query': query,
-                    'limit': 1,
+                    'limit': 10,
                     'indent': true,
                     'key': 'AIzaSyAf3z_eNF3RKsZxoy7SXEGPD3v-9bNfgfQ',
                 };
-
+                let tempresults = {};
                 $.getJSON(service_url + '?callback=?', params, function (response) {
                     console.log('Response for Research: ' + JSON.stringify(response));
-                    this.researchResult = response;
-                    $.each(response.itemListElement, function (i, element) {
-                        document.getElementById("resultfield").innerHTML = "<img src=\""
-                            + element['result']['image']["contentUrl"] + "\"> " + "<br />"
-                            + element['result']['name'] + "<br />"
-                            + element['result']['description'] + "<br />"
-                            + element['result']['detailedDescription']['articleBody'] + "<br />"
-                            + "<a href=\" + element['result']['detailedDescription']['url']\">Mehr info</a>";
-                    });
-                }); //TODO: use socket emit for database handling
+                    this.researchresults.push(response);
+                    console.log('Response for Research222222222222222222: ' + JSON.stringify(this.researchresults));
+                    // $.each(response.itemListElement, function (i, element) {
+                    //     document.getElementById("resultfield").innerHTML = "<img src=\""
+                    //         + element['result']['image']["contentUrl"] + "\"> " + "<br />"
+                    //         + element['result']['name'] + "<br />"
+                    //         + element['result']['description'] + "<br />"
+                    //         + element['result']['detailedDescription']['articleBody'] + "<br />"
+                    //         + "<a href=\" + element['result']['detailedDescription']['url']\">Mehr info</a>";
+                    // });
+                });
+
+
+                //TODO: use socket emit for database handling
                 //TODO: establish Connection -> get Response /result
                 // this.googleResponse=displayedResult;
                 //TODO: sent results to server
-            }
+            },
         },
         watch: {
             researchmode: function (mode) {
                 //if (mode === 'Info') {
-                console.log('researchmode was changed to:'+ mode);
-                    this.searchGoogle('Taylor Swift');
+                console.log('researchmode was changed to:' + mode);
+                this.searchGoogle('Taylor Swift');
                 //}
             }
+        },
+        components: {
+            researchresult
         }
     }
 </script>
