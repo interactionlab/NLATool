d475ce04f9f58cba22755b793b3af82390d025ad
telion2 <telion.official1@hotmail.com>
2018-01-09 15:21:07 +0100

    changes for title changes

--- views/components/profile/editdocument.vue
+++ views/components/profile/editdocument.vue
@@ -1,17 +1,18 @@
 <template>
     <div class="mdl-grid">
+        <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--8-col contentColor">
         <input type="text"
-               v-model="newTitle"
+               v-model="newtitle"
                v-on:keyup.enter="edit"
-               v-bind:value="document.name"
-               class="mdl-cell mdl-cell--8-col contentColor"/>
+               v-on:keyup.esc="back"
+               class="mdl-textfield__input"/>
+        </div>
         <div class="mdl-cell mdl-cell--4-col contentColor">
             <button class="mdl-button mdl-js-button mdl-js-ripple-effect"
                     v-on:click="edit"
             >Save</button>
             <button class="mdl-button mdl-js-button mdl-js-ripple-effect"
                     v-on:click="back"
-                    id="noteMenu"
             >Back</button>
         </div>
     </div>
@@ -20,26 +21,31 @@
 <script>
     export default {
         props: {
-            document: Object
+            document: Object,
+            newtitle: String
         },
         data: function () {
             return {
                 document: this.document,
                 changing: 'showdoc',
-                newTitle: ''
+                newtitle: this.newtitle
             }
         },
         methods: {
             edit: function () {
                 let socket = io('http://localhost:8081');
-                socket.emit('changeTitle', this.document.docID, this.newTitle);
-                this.$emit('editing', this.newTitle);
+
+                this.document.name = this.newtitle;
+                console.log('Title Changed: ' + this.document.name);
+                socket.emit('changeTitle', this.document.docID, this.newtitle);
+                this.$emit('editing', this.newtitle);
             },
             back: function(){
                 this.$emit('editing', this.document.name);
             }
         },
-        computed: {}
+        computed: {
+        }
     }
 </script>
 
