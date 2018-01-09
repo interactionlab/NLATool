72e1ab1f4b32f930eb10047fc4a05bd9bd9bb9d8
LuxyNerd <emine@live.com.my>
2018-01-09 19:56:06 +0100

    marking result from resultList if mouse over

--- views/components/analysis/researchresult.vue
+++ views/components/analysis/researchresult.vue
@@ -1,5 +1,9 @@
-<template>
-    <div>
+<template> <!--editordocument in 8080-->
+    <div v-bind:class="{researchresulthover: hover}"
+         v-on:mouseout="accentuate"
+         v-on:mouseover="accentuate"
+         v-on:click="selectResult">
+
         <div><img v-if="typeof researchresult.result.image !== 'undefined'"
                   v-bind:src="researchresult.result.image.contentUrl"/></div>
         <div v-if="typeof researchresult.result !== 'undefined'">{{researchresult.result.name}}</div>
@@ -14,12 +18,24 @@
     export default {
         props: {
             researchresult: Object,
-            researchresults: Object
+            researchresults: Object,
+            index: Number
         },
         data: function () {
             return {
                 researchresult: this.researchresult,
-                researchresults: this.researchresults
+                researchresults: this.researchresults,
+                hover: false,
+                index: this.index
+            }
+        },
+        methods: {
+            accentuate: function () {
+                this.hover = !this.hover;
+            },
+            selectResult: function () {
+
+                this.$emit('selectresult', this.index);
             }
         }
     }
