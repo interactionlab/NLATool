41e3e7ed3a9a17f652c691be5048c040f1e3c97e
LuxyNerd <emine@live.com.my>
2018-01-09 15:08:30 +0100

    marged again

--- views/components/analysis/researchresult.vue
+++ views/components/analysis/researchresult.vue
@@ -1,26 +1,14 @@
-<template>
-<<<<<<< HEAD
-    <!-- Shows the image-->
-    <div><img v-bind:src="researchresult.image.contentUrl"/></div>
-    <!-- Shows a Name -->
-    <div>{{researchresult.result.name}}</div>
-    <!-- Shows a short description -->
-    <div> {{researchresult.description}}</div>
-    <!-- Shows a longer description -->
-    <div> {{researchresult.detailedDescription.articleBody}}</div>
-    <!-- Shows the source -->
-    <div>{{research}}</div>
-=======
+<templat>
     <div>
         <div><img v-if="typeof researchresult.result.image !== 'undefined'"
                   v-bind:src="researchresult.result.image.contentUrl"/></div>
-
         <div v-if="typeof researchresult.result !== 'undefined'">{{researchresult.result.name}}</div>
-        <div v-if="typeof researchresult.result.description !== 'undefined'"> {{researchresult.result.description}}</div>
-        <div v-if="typeof researchresult.result.detailedDescription !== 'undefined'"> {{researchresult.result.detailedDescription.articleBody}}</div>
+        <div v-if="typeof researchresult.result.description !== 'undefined'">{{researchresult.result.description}}</div>
+        <div v-if="typeof researchresult.result.detailedDescription !== 'undefined'">
+            {{researchresult.result.detailedDescription.articleBody}}
+        </div>
     </div>
->>>>>>> 7042b261b310ebf506e3b132556f84e791e3ed8a
-</template>
+</templat>
 
 <script>
     export default {
