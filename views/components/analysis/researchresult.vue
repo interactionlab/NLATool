b5396e88d5a850b4d805066c19840c7620bfb091
LuxyNerd <emine@live.com.my>
2018-01-15 14:46:27 +0100

    results values looks separated now

--- views/components/analysis/researchresult.vue
+++ views/components/analysis/researchresult.vue
@@ -1,17 +1,35 @@
 <template> <!--editordocument in 8080-->
-    <div v-bind:class="{researchresulthover: hover}"
-         v-on:mouseout="accentuate"
-         v-on:mouseover="accentuate"
-         v-on:click="selectResult">
+    <main class="mdl-layout__content">
+        <div class="mdl-grid">
+            <!-- Bottons for localisation-->
+            <div class="mdl-cell mdl-cell--10-col">
+                <div v-bind:class="{researchresulthover: hover}"
+                     v-on:mouseout="accentuate"
+                     v-on:mouseover="accentuate"
+                     v-on:click="selectResult">
 
-        <div><img v-if="typeof researchresult.result.image !== 'undefined'"
-                  v-bind:src="researchresult.result.image.contentUrl"/></div>
-        <div v-if="typeof researchresult.result !== 'undefined'">{{researchresult.result.name}}</div>
-        <div v-if="typeof researchresult.result.description !== 'undefined'">{{researchresult.result.description}}</div>
-        <div v-if="typeof researchresult.result.detailedDescription !== 'undefined'">
-            {{researchresult.result.detailedDescription.articleBody}}
+                    <div><img v-if="typeof researchresult.result.image !== 'undefined'"
+                              v-bind:src="researchresult.result.image.contentUrl"
+                              id="imageid"/>
+                    </div>
+
+                    <div v-if="typeof researchresult.result !== 'undefined'"
+                         id="nameid">
+                        {{researchresult.result.name}}
+                    </div>
+
+                    <div v-if="typeof researchresult.result.description !== 'undefined'"
+                         id="shortdescrid">
+                    </div>
+
+                    <div v-if="typeof researchresult.result.detailedDescription !== 'undefined'"
+                         id="longdescrid">
+                        {{researchresult.result.detailedDescription.articleBody}}
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+    </main>
 </template>
 
 <script>
@@ -35,7 +53,7 @@
             },
             selectResult: function () {
                 let soket = io('http://localhost:8080');
-               // socket.$emit('selectresult',this.index);
+                // socket.$emit('selectresult',this.index);
                 this.$emit('selectresult', this.index);
             }
         }
