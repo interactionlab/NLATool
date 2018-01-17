01138bdbf1412f7c50a13684f20e7576dec53149
telion2 <telion.official1@hotmail.com>
2018-01-17 11:17:18 +0100

    tidied up transactionclass.js. this file is not used tho.

--- modules/transactionclass.js
+++ modules/transactionclass.js
@@ -7,55 +7,58 @@ const dbStub = require('./db_stub');
 const wait = require('wait.for');
 const jsonAction = require('./json_actions');
 
-let Transaction = class Transaction{
-      constructor(connection){
-          this.querys = {};
-          this.connection = connection;
-      }
+let Transaction = class Transaction {
+    constructor(connection) {
+        this.querys = {};
+        this.connection = connection;
+    }
 
-      transact(){
-          this.connection.beginTransaction(function(err){
-              if (err){throw err;}
-          });
-      }
-
-
-
-      get allQuerys(){
-          return this.querys;
-      }
-      setNewConnection(conn){
-          this.connection = conn;
-      }
+    transact() {
+        this.connection.beginTransaction(function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 
 
+    get allQuerys() {
+        return this.querys;
+    }
 
-    connection.beginTransaction(function(err) {
-        if (err) { throw err; }
-        connection.query('INSERT INTO posts SET title=?', title, function (error, results, fields) {
+    setNewConnection(conn) {
+        this.connection = conn;
+    }
+};
+
+//Example from documentation
+connection.beginTransaction(function (err) {
+    if (err) {
+        throw err;
+    }
+    connection.query('INSERT INTO posts SET title=?', title, function (error, results, fields) {
+        if (error) {
+            return connection.rollback(function () {
+                throw error;
+            });
+        }
+
+        var log = 'Post ' + results.insertId + ' added';
+
+        connection.query('INSERT INTO log SET data=?', log, function (error, results, fields) {
             if (error) {
-                return connection.rollback(function() {
+                return connection.rollback(function () {
                     throw error;
                 });
             }
-
-            var log = 'Post ' + results.insertId + ' added';
-
-            connection.query('INSERT INTO log SET data=?', log, function (error, results, fields) {
-                if (error) {
-                    return connection.rollback(function() {
-                        throw error;
+            connection.commit(function (err) {
+                if (err) {
+                    return connection.rollback(function () {
+                        throw err;
                     });
                 }
-                connection.commit(function(err) {
-                    if (err) {
-                        return connection.rollback(function() {
-                            throw err;
-                        });
-                    }
-                    console.log('success!');
-                });
+                console.log('success!');
             });
         });
     });
-};
+});
