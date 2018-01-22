719d7bd7a0acb796ce55cd8fb221bc376316c1cf
telion2 <telion.official1@hotmail.com>
2018-01-22 11:17:50 +0100

    synch commit

--- views/components/analysis/researchresult.vue
+++ views/components/analysis/researchresult.vue
@@ -32,6 +32,7 @@
                         </div>
                     </div>
 
+
                     <div class="mdl-grid">
                         <div class="mdl-cell mdl-cell--2-col">
                             <button class="mdl-button mdl-js-button"
