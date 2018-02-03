4bd5c50e070890193da84569be45846558398a97
LuxyNerd <emine@live.com.my>
2018-02-03 12:20:05 +0100

    created component for maps - error handling needed

---
+++ views/components/analysis/map.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- input field-->
+    <div class="mdl-layout__content contentColor"
+         ref="map">
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props:{
+
+        },
+        data:function () {
+            return{
+
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
