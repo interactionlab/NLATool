9043205ccec4788dd7ad813a3d663ec4ae235c91
LuxyNerd <emine@live.com.my>
2018-02-05 15:14:36 +0100

    binded map

--- views/components/analysis/googlemap.vue
+++ views/components/analysis/googlemap.vue
@@ -1,10 +1,24 @@
 <template>
-    
+    <div  class="googlemap":id="googlemapname"></div>
 </template>
 
 <script>
     export default {
-        name: "googlemap"
+        props: ['name'],
+        data: function () {
+            return {
+                googlemapname: this.name + "-map",
+            }
+        },
+        mounted: function () {
+            console.log('MapName: '+ this.googlemapname);
+            const element = document.getElementById(this.googlemapname);
+            const options = {
+                zoom: 14,
+                center: new google.maps.LatLng(51.501527, -0.1921837)
+            };
+            const map = new google.maps.Map(element, options);
+        }
     }
 </script>
 
