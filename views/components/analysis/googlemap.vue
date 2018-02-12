cd511abf027ac0f64bccf77c1e65f6f08f2f1ec5
LuxyNerd <emine@live.com.my>
2018-02-12 11:56:55 +0100

    prepared MapCoordinates, because we do not get informations to the map

--- views/components/analysis/googlemap.vue
+++ views/components/analysis/googlemap.vue
@@ -1,5 +1,5 @@
 <template>
-    <div  class="googlemap":id="googlemapname"></div>
+    <div class="googlemap" :id="googlemapname"></div>
 </template>
 
 <script>
@@ -8,16 +8,25 @@
         data: function () {
             return {
                 googlemapname: this.name + "-map",
+                mapoptions: {
+                    zoom: 14,
+                    center: new google.map.LatLng(51.501527, -0.1921837)
+                }
             }
         },
-        mounted: function () {
-            console.log('MapName: '+ this.googlemapname);
+        mounted() {
+            console.log('MapName: ' + this.googlemapname);
             const element = document.getElementById(this.googlemapname);
-            const options = {
+            const mapoptions = {
                 zoom: 14,
                 center: new google.maps.LatLng(51.501527, -0.1921837)
             };
             const map = new google.maps.Map(element, options);
+        },
+        methods:{
+            setmapcoordinates: function (x,y) {
+                this.mapoptions.center = new google.map.LatLng(x, y);
+            }
         }
     }
 </script>
