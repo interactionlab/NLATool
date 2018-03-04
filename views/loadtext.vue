4542d8536f2ddcd850dbeaaab0462262148b836e
Sven Mayer <mayer1802@gmail.com>
2018-03-04 22:29:04 +0100

    fixed #53

--- views/loadtext.vue
+++ views/loadtext.vue
@@ -12,20 +12,23 @@
         <main class="mdl-layout__content" style="display: flex; flex-flow: row wrap; width: 100%;">
             <form  class="mdl-grid" style="max-width:2160px; overflow-y:auto; flex-direction:column; width:100%" action="/loadWrittenText" method="post">
                 <!-- Textfield for title-->
-                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col contentColor" style="flex:0">
-                    <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
-                    <label class="mdl-textfield__label" for="titlebox">Enter title here ...</label>
+                <div class="mdl-cell mdl-cell--12-col contentColor" style="flex:0;padding:0.5em 1em 0em 1em">
+                    <div class="mdl-textfield mdl-js-textfield" style="width:100%;">
+                        <input class="mdl-textfield__input" type="text" id="titlebox" name="title">
+                        <label class="mdl-textfield__label" for="titlebox">Enter title here ...</label>
+                    </div>
                 </div>
                 <div class="mdl-layout-spacer mdl-cell--12-col" style="flex:0"></div>
 
                 <!-- Textfield for textinput -->
-                <div class="mdl-textfield mdl-js-textfield mdl-cell mdl-cell--12-col contentColor"
-                     style="flex:2 0px;max-height: 100%; height:auto !important; display:flex">
-            <textarea class="mdl-textfield__input" type="text" id="textbox" name="textInput" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" data-gramm="false">
-            </textarea>
+                <div class="mdl-cell mdl-cell--12-col contentColor"
+                     style="flex:2 0px;max-height: 100%; height:auto !important; display:flex;padding:0.5em 1em 0em 1em">
+                     <div class="mdl-textfield mdl-js-textfield" style="width:100%;max-height: 100%; height:auto !important; display:flex;">
+                        <textarea class="mdl-textfield__input" type="text" id="textbox" name="textInput" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" data-gramm="false"></textarea>
                     <label class="mdl-textfield__label" for="textbox">
                         Enter text here ...
                     </label>
+                    </div>
                 </div>
                 <!-- Analyze Button -->
                 <div class="mdl-cell mdl-cell--12-col" style="flex:0; text-align: center">
