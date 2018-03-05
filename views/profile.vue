d808a22006752e9258a317aedf1e28a339fc43a8
Sven Mayer <mayer1802@gmail.com>
2018-03-05 01:13:54 +0100

    fixed #49 - finaly the text overview list looks nice

--- views/profile.vue
+++ views/profile.vue
@@ -11,28 +11,31 @@
         <component is="headernavbar"
                    v-bind:title_small="title_small">
         </component>
-        <main class="mdl-layout__content">
-            <div style="display: table">
-                <form action="/profile/loadMoreDocuments"
-                      method="post"
-                      v-for="numberOfButton in amountOfButtons"
-                      style="display: table-cell;">
-                    <button name="numberOfButton"
-                            v-bind:value="numberOfButton"
-                            class="mdl-button mdl-js-button mdl-js-ripple-effect"
-                            style="display: inline"
-                    >{{numberOfButton}}
-                    </button>
-                </form>
-            </div>
-            <div class="mdl-grid" style="max-width:2160px;">
-                <component is="document"
-                           v-for="document in documents"
-                           v-bind:document="document"
-                           v-bind:key="document.docID"
-                           v-on:deleted="deleted($event)"
-                           class="mdl-cell--12-col">
-                </component>
+        
+        <main class="mdl-layout__content" style="justify-content: center; align-items: center;">
+            <div class="mdl-card mdl-shadow--6dp" style="margin: auto; overflow: initial; max-width: 500px;width: 100%;">
+       
+                <ul class="mdl-list">
+                    <form action="/profile/loadMoreDocuments"
+                          method="post"
+                          v-for="numberOfButton in amountOfButtons"
+                          style="display: table-cell;">
+                        <button name="numberOfButton"
+                                v-bind:value="numberOfButton"
+                                class="mdl-list__item mdl-button mdl-js-button mdl-js-ripple-effect"
+                                style="display: inline"
+                        >{{numberOfButton}}
+                        </button>
+                    </form>
+                </ul>
+                <ul class="mdl-list" style="max-width:2160px;">
+                    <component is="document"
+                               v-for="document in documents"
+                               v-bind:document="document"
+                               v-bind:key="document.docID"
+                               v-on:deleted="deleted($event)">
+                    </component>
+                </ul>
             </div>
         </main>
     </div>
