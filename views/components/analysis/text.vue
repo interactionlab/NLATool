da2a9155df130c887e0ba12af74972e4f63706f6
telion2 <telion.official1@hotmail.com>
2018-03-09 23:24:10 +0100

    optimized brackets.

--- views/components/analysis/text.vue
+++ views/components/analysis/text.vue
@@ -4,8 +4,8 @@
           v-on:mouseup="endSelection"
           v-on:mouseover="tohover = true"
           v-on:mouseout="stophover">
-        <span class="nonPreAlt specialBracket" v-bind:class="toHighlight">{{beginBrackets}}</span><span
-            class="nonPreAlt" v-bind:class="toHighlight" ref="word" v-on:mouseover="hover">{{token.content}}</span><span class="nonPreAlt specialBracket" v-bind:class="toHighlight">{{endBrackets}}</span><span class="preAlt" v-bind:class="classToHighlightGap">{{getWordGap}}</span></span>
+        <span v-if="token.coref !== undefined" class="nonPreAlt specialBracket" v-bind:class="toHighlight">{{beginBrackets}}</span><span
+            class="nonPreAlt" v-bind:class="toHighlight" ref="word" v-on:mouseover="hover">{{token.content}}</span><span class="nonPreAlt specialBracket" v-bind:class="toHighlight" v-if="token.coref !== undefined">{{endBrackets}}</span><span class="preAlt" v-bind:class="classToHighlightGap">{{getWordGap}}</span></span>
 </template>
 <script>
     export default {
