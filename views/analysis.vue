27d58fb022bf3256c70cdc0e3253a4469eaabf0e
Sven Mayer <mayer1802@gmail.com>
2018-03-10 02:16:07 +0100

    removed another token list - not needed

--- views/analysis.vue
+++ views/analysis.vue
@@ -390,7 +390,6 @@
         beforeDestroy() {
             window.removeAllListeners();
         },
-        computed: {},
         watch: {
             moreData: {
                 handler: function (newData) {
