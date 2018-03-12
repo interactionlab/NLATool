6306f1a8659e45cabc1a04389975a88436da1884
Sven Mayer <mayer1802@gmail.com>
2018-03-12 23:38:07 +0100

    updates for #80 and added page count above the page

--- views/components/analysis/toolbar/textviewcontrol.vue
+++ views/components/analysis/toolbar/textviewcontrol.vue
@@ -1,15 +1,39 @@
 <template>
     <div class="mdl_grid" style="position: relative;">
-        <div class="mdl-cell mdl-cell--6-col scopeButton icon-arrow-left" v-on:click="changeScope(true)"></div>
-        <div class="mdl-cell mdl-cell--6-col scopeButton icon-arrow-right" style="text-align: right;" id="forwardScopeButton" v-on:click="changeScope(false)">
+        <div class="mdl-cell mdl-cell--5-col scopeButton"
+            v-bind:class="{'icon-arrow-left': leftenabled }"
+            v-on:click="changeScope(true)"></div>
+        <div class="mdl-cell mdl-cell--2-col" style="display: inline-block;">Page {{currentpage}} of {{pagecount}}</div>
+        <div class="mdl-cell mdl-cell--5-col scopeButton " style="text-align: right;"
+            v-bind:class="{'icon-arrow-right': rightenabled }"
+            v-on:click="changeScope(false)">
         </div>
     </div>
 </template>
 <script>
     export default {
+        props: {
+            currentpage: { type: Number, default: 0 },
+            pagecount: { type: Number, default: 0 },
+        },
         data: function () {
             return {
-                numberofcolumns: 1,
+                leftenabled: false,
+                rightenabled: true,
+            }
+        },
+        watch:{
+            currentpage: function(){
+                if (this.currentpage === 1){
+                    this.leftenabled = false;
+                } else {
+                    this.leftenabled = true;
+                }
+                if (this.currentpage === this.pagecount) {
+                    this.rightenabled = false;
+                } else {
+                    this.rightenabled = true;
+                }
             }
         },
         methods: {
