c0c73e3fa3ae5cc8eea191e7fca840142ef705b6
Sven Mayer <mayer1802@gmail.com>
2018-03-14 23:57:52 +0100

    fixed #58 - please have a look that you are well represened in the impring

--- views/imprint.vue
+++ views/imprint.vue
@@ -8,55 +8,15 @@
         <component is="headernavbar"
                    v-bind:title_small="title_small">
         </component>
-        <div class="mdl-layout__content">
-            <h1>Impressum</h1>
-            <h2>Angaben gem&auml;&szlig; &sect; 5 TMG:</h2>
-            <p>Muster
-                Universit&auml;t<br/> Musterstra&szlig;e 111<br/> Geb&auml;ude 44<br/> 90210 Musterstadt</p>
-            <h2>Vertreten durch:</h2>
-            <p>Herr Dr. Harry Mustermann<br/> Frau Luise Beispiel</p>
-            <h2>Kontakt:</h2>
-            <p>Telefon: +49 (0) 123 44 55 66<br/> Telefax: +49 (0) 123 44 55 99<br/> E-Mail:
-                mustermann@musterfirma.de</p>
-            <h2>Umsatzsteuer:</h2>
-            <p>Umsatzsteuer-Identifikationsnummer
-                gem&auml;&szlig; &sect;27 a Umsatzsteuergesetz:<br/> DE 999 999 999</p>
-            <h2>Streitschlichtung</h2>
-            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor
-                einer Verbraucherschlichtungsstelle teilzunehmen.</p>
-            <h2>Haftung f&uuml;r Inhalte</h2>
-            <p>Als Diensteanbieter sind wir gem&auml;&szlig; &sect; 7 Abs.1 TMG f&uuml;r eigene Inhalte auf diesen
-                Seiten
-                nach den allgemeinen Gesetzen verantwortlich. Nach &sect;&sect; 8 bis 10 TMG sind wir als
-                Diensteanbieter jedoch nicht verpflichtet, &uuml;bermittelte oder gespeicherte fremde Informationen zu
-                &uuml;berwachen oder nach Umst&auml;nden zu forschen, die auf eine rechtswidrige T&auml;tigkeit
-                hinweisen.</p>
-            <p>Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach
-                den allgemeinen Gesetzen bleiben hiervon unber&uuml;hrt. Eine diesbez&uuml;gliche Haftung ist jedoch
-                erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung m&ouml;glich. Bei Bekanntwerden
-                von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
-            <h2>Haftung f&uuml;r Links</h2>
-            <p>Unser Angebot enth&auml;lt Links zu externen Websites Dritter, auf
-                deren Inhalte wir keinen Einfluss haben. Deshalb k&ouml;nnen wir f&uuml;r diese fremden Inhalte auch
-                keine Gew&auml;hr &uuml;bernehmen. F&uuml;r die Inhalte der verlinkten Seiten ist stets der jeweilige
-                Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
-                Verlinkung auf m&ouml;gliche Rechtsverst&ouml;&szlig;e &uuml;berpr&uuml;ft. Rechtswidrige Inhalte
-                waren zum Zeitpunkt der Verlinkung nicht erkennbar.</p>
-            <p>Eine permanente inhaltliche Kontrolle der
-                verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-                Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
-            <h2>Urheberrecht</h2>
-            <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten
-                unterliegen dem deutschen Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede
-                Art der Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen
-                Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r
-                den privaten, nicht kommerziellen Gebrauch gestattet.</p>
-            <p>Soweit die Inhalte auf dieser Seite nicht
-                vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
-                Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam
-                werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
-                werden wir derartige Inhalte umgehend entfernen.</p>
-            <p>Quelle: <a href="https://www.erecht24.de">https://www.e-recht24.de</a></p>
+        <main class="mdl-layout__content" style="padding: 1em; display: inline-block; text-align: center;">
+            <div  class="mdl-grid contentColor mdl-shadow--6dp" style="max-width:2160px; overflow-y:auto;     display: inline-block; text-align: left;">
+                <h1>Impressum</h1>
+                <h2>Project: NLA - Natural Language Analyse Tool</h2>
+                <p>This project was developed as a student project in the summer and winter term of 2017/18 at the <a href="https://www.uni-stuttgart.de/en/">University of Stuttgart</a> by Eric Hämmerle, Emine Turcan,  Florin Rheinwald, and Gustav Murawski. The project was supervised by <a href="http://sven-mayer.com">Sven Mayer</a>, <a href="http://vali.de">Valentin Schwind</a>, <a href="http://www.ims.uni-stuttgart.de/institut/mitarbeiter/gaertnms/">Markus Gärtner</a>, and <a href="http://www.ims.uni-stuttgart.de/institut/mitarbeiter/jonas/">Jonas Kuhn</a>. During the project, Markus Gärtner and Jonas Kuhn have been responsible for the computational linguistics parts. Sven Mayer and Valentin Schwind were responsible for technical implementation and user experience (UX).
+                </p>
+                <p>To report any issues, feature request or any other form of feedback please create at issue at <a href="https://github.com/interactionlab/NLATool/issues">github.com</a></p>
+                <p>The source code of this project is under GPL-3.0 Licence avalible at: <a href="github.com/interactionlab/NLATool">github.com/interactionlab/NLATool</a></p>
+            </div>
         </div>
     </div>
 </template>
