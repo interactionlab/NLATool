1ce841c9a2f01b7d7a795e1fcb47843c7db750d8
telion2 <telion.official1@hotmail.com>
2018-03-15 18:31:38 +0100

    #93 preventing scrolls if element is already in view. It still has wierd scroll behavior.

--- views/components/analysis/text.vue
+++ views/components/analysis/text.vue
@@ -25,7 +25,6 @@
                     return []
                 }
             },
-            allowtexttoscroll: {type: Boolean, default: false}
         },
         data: function () {
             return {
@@ -35,6 +34,15 @@
             }
         },
         computed: {
+            allowtexttoscroll: function () {
+                let rect = this.$el.getBoundingClientRect();
+                return !(
+                    rect.top >= 0 &&
+                    rect.left >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+                    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+                );
+            },
             tobejumped: function () {
                 if (this.index === this.selectedindexes.start) {
                     return this.index;
