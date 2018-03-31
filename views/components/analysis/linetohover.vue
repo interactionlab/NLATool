ba5ad3ba7241f9ebddf29320a2a25fc9cb0daee5
SamyStyle <mayer1802@gmail.com>
2018-03-31 19:22:46 +0200

    fixed hover line gap

--- views/components/analysis/linetohover.vue
+++ views/components/analysis/linetohover.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <div style="height: 2px;position: absolute; z-index: 11;"
-        v-bind:style="{top : offsetstart.top + offsetstart.height +3 +'px', left : offsetstart.left + offsetstart.width/2.0 +'px', width: offsetend.left - 12 - offsetstart.left -offsetstart.width/2.0+'px'}" v-bind:class="semanticclass"></div>
+        v-bind:style="{top : offsetstart.top + offsetstart.height +'px', left : offsetstart.left + offsetstart.width/2.0 +'px', width: offsetend.left - 12 - offsetstart.left -offsetstart.width/2.0+'px'}" v-bind:class="semanticclass"></div>
         
-        <div style="width: 2px;height: 5px;position: absolute;z-index: 11;" v-bind:style="{top : offsetstart.top + offsetstart.height -2 +'px', left : offsetstart.left + offsetstart.width/2.0 +'px'}" v-bind:class="semanticclass"></div>
+        <div style="width: 2px;height: 5px;position: absolute;z-index: 11;" v-bind:style="{top : offsetstart.top + offsetstart.height -5 +'px', left : offsetstart.left + offsetstart.width/2.0 +'px'}" v-bind:class="semanticclass"></div>
         
-        <div style="width: 2px;position: absolute;z-index:  11;" v-bind:style="{top : Math.min(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height +3) +'px', left : offsetend.left - 14 +'px', height: Math.max(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height +3) - Math.min(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height +3)+'px'}" v-bind:class="semanticclass"></div>
+        <div style="width: 2px;position: absolute;z-index:  11;" v-bind:style="{top : Math.min(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height) +'px', left : offsetend.left - 14 +'px', height: Math.max(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height) - Math.min(offsetend.top + offsetend.height/2.0, offsetstart.top + offsetstart.height)+'px'}" v-bind:class="semanticclass"></div>
         
         <div style="width: 14px;height: 2px;position: absolute;z-index: 11" v-bind:style="{top : offsetend.top + offsetend.height/2.0 +'px', left : offsetend.left - 14 +'px'}" v-bind:class="semanticclass"></div>
     
